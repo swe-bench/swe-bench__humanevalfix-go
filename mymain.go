1f80152ae35edab14cffb2dd2bc31443c7a479ff
swe-bench <>
2024-06-12 16:19:28 -0400

    Task instance Go/43

--- mymain.go
+++ mymain.go
@@ -5,28 +5,49 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
-// Return list with elements incremented by 1.
-// >>> IncrList([1, 2, 3])
-// [2, 3, 4]
-// >>> IncrList([5, 3, 5, 2, 3, 3, 9, 0, 123])
-// [6, 4, 6, 3, 4, 4, 10, 1, 124]
-func IncrList(l []int) []int {
+// PairsSumToZero takes a list of integers as an input.
+// it returns true if there are two distinct elements in the list that
+// sum to zero, and false otherwise.
+// >>> PairsSumToZero([1, 3, 5, 0])
+// false
+// >>> PairsSumToZero([1, 3, -2, 1])
+// false
+// >>> PairsSumToZero([1, 2, 3, 7])
+// false
+// >>> PairsSumToZero([2, 4, -5, 3, 5, 7])
+// true
+// >>> PairsSumToZero([1])
+// false
+func PairsSumToZero(l []int) bool {
 
-    n := len(l)
-	for i := 1; i < n; i++ {
-		l[i]++
+    seen := map[int]bool{}
+	for i := 0; i < len(l); i++ {
+		for j := i; j < len(l); j++ {
+			if l[i] + l[j] == 0 {
+				if _, ok := seen[l[i]]; !ok {
+					seen[l[i]] = true
+					return true
+				}
+				if _, ok := seen[l[j]]; !ok {
+					seen[l[j]] = true
+					return true
+				}
+			}
+		}
 	}
-	return l
+	return false
 }
 
-func ExampleTestIncrList(t *testing.T) {
+func ExampleTestPairsSumToZero(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([]int{2, 3, 4}, IncrList([]int{1, 2, 3}))
-    assert.Equal([]int{6, 3, 6, 3, 4, 4, 10, 1, 124}, IncrList([]int{5, 2, 5, 2, 3, 3, 9, 0, 123}))
+    assert.Equal(false, PairsSumToZero([]int{1, 3, 5, 0}))
+    assert.Equal(false, PairsSumToZero([]int{1, 3, -2, 1}))
+    assert.Equal(false, PairsSumToZero([]int{1, 2, 3, 7}))
+    assert.Equal(true, PairsSumToZero([]int{2, 4, -5, 3, 5, 7}))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestIncrList(t)
+    ExampleTestPairsSumToZero(t)
 }
