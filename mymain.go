c915a24687b1ca79de894b0651c6ffa08d59de98
swe-bench <>
2024-06-12 16:19:29 -0400

    Task instance Go/44

--- mymain.go
+++ mymain.go
@@ -4,50 +4,36 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
+import (
+    "strconv"
+)
 
-// PairsSumToZero takes a list of integers as an input.
-// it returns true if there are two distinct elements in the list that
-// sum to zero, and false otherwise.
-// >>> PairsSumToZero([1, 3, 5, 0])
-// false
-// >>> PairsSumToZero([1, 3, -2, 1])
-// false
-// >>> PairsSumToZero([1, 2, 3, 7])
-// false
-// >>> PairsSumToZero([2, 4, -5, 3, 5, 7])
-// true
-// >>> PairsSumToZero([1])
-// false
-func PairsSumToZero(l []int) bool {
+// Change numerical base of input number x to base.
+// return string representation after the conversion.
+// base numbers are less than 10.
+// >>> ChangeBase(8, 3)
+// '22'
+// >>> ChangeBase(8, 2)
+// '1000'
+// >>> ChangeBase(7, 2)
+// '111'
+func ChangeBase(x int, base int) string {
 
-    seen := map[int]bool{}
-	for i := 0; i < len(l); i++ {
-		for j := i; j < len(l); j++ {
-			if l[i] + l[j] == 0 {
-				if _, ok := seen[l[i]]; !ok {
-					seen[l[i]] = true
-					return true
-				}
-				if _, ok := seen[l[j]]; !ok {
-					seen[l[j]] = true
-					return true
-				}
-			}
-		}
-	}
-	return false
+    if x >= base {
+        return ChangeBase(x*base, base) + ChangeBase(x%base, base)
+    }
+    return strconv.Itoa(x)
 }
 
-func ExampleTestPairsSumToZero(t *testing.T) {
+func ExampleTestChangeBase(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(false, PairsSumToZero([]int{1, 3, 5, 0}))
-    assert.Equal(false, PairsSumToZero([]int{1, 3, -2, 1}))
-    assert.Equal(false, PairsSumToZero([]int{1, 2, 3, 7}))
-    assert.Equal(true, PairsSumToZero([]int{2, 4, -5, 3, 5, 7}))
+    assert.Equal("22", ChangeBase(8, 3))
+    assert.Equal("1000", ChangeBase(8, 2))
+    assert.Equal("111", ChangeBase(7, 2))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestPairsSumToZero(t)
+    ExampleTestChangeBase(t)
 }
