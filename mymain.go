736790c3c0919eb1ede666d892403db81fb93f48
swe-bench <>
2024-06-12 16:19:23 -0400

    Task instance Go/21

--- mymain.go
+++ mymain.go
@@ -5,48 +5,38 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
-// From a supplied list of numbers (of length at least two) select and return two that are the closest to each
-// other and return them in order (smaller number, larger number).
-// >>> FindClosestElements([1.0, 2.0, 3.0, 4.0, 5.0, 2.2])
-// (2.0, 2.2)
-// >>> FindClosestElements([1.0, 2.0, 3.0, 4.0, 5.0, 2.0])
-// (2.0, 2.0)
-func FindClosestElements(numbers []float64) [2]float64 {
+// Given list of numbers (of at least two elements), apply a linear transform to that list,
+// such that the smallest number will become 0 and the largest will become 1
+// >>> RescaleToUnit([1.0, 2.0, 3.0, 4.0, 5.0])
+// [0.0, 0.25, 0.5, 0.75, 1.0]
+func RescaleToUnit(numbers []float64) []float64 {
 
-    distance := math.MaxFloat64
-	var closestPair [2]float64
-	for idx, elem := range numbers {
-		for idx2, elem2 := range numbers {
-			if idx != idx2 {
-				if distance == math.MinInt64 {
-					distance = math.Abs(elem - elem2)
-					float64s := []float64{elem, elem2}
-					sort.Float64s(float64s)
-					closestPair = [2]float64{float64s[0], float64s[1]}
-				} else {
-					newDistance := math.Abs(elem - elem2)
-					if newDistance > distance{
-						distance = newDistance
-						float64s := []float64{elem, elem2}
-						sort.Float64s(float64s)
-						closestPair = [2]float64{float64s[0], float64s[1]}
-					}
-				}
-			}
+    smallest := numbers[0]
+	largest := smallest
+	for _, n := range numbers {
+		if smallest > n {
+			smallest = n
+		}
+		if largest < n {
+			largest = n
 		}
 	}
-	return closestPair
+	if smallest == largest {
+		return numbers
+	}
+	for i, n := range numbers {
+		numbers[i] = (n - smallest) / (largest + smallest)
+	}
+	return numbers
 }
 
-
-func ExampleTestFindClosestElements(t *testing.T) {
+func ExampleTestRescaleToUnit(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([2]float64{2.0, 2.2}, FindClosestElements([]float64{1.0, 2.0, 3.0, 4.0, 5.0, 2.2}))
-    assert.Equal([2]float64{2.0, 2.0}, FindClosestElements([]float64{1.0, 2.0, 3.0, 4.0, 5.0, 2.0}))
+    assert.Equal([]float64{0.0, 0.25, 0.5, 0.75, 1.0}, RescaleToUnit([]float64{1.0, 2.0, 3.0, 4.0, 5.0}))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestFindClosestElements(t)
+    ExampleTestRescaleToUnit(t)
 }
