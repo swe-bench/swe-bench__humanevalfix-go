658fbfbe1e2dd42486ab85188d76bcbd76163e47
swe-bench <>
2024-06-12 16:19:37 -0400

    Task instance Go/78

--- mymain.go
+++ mymain.go
@@ -4,38 +4,46 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
-import (
-    "math"
-)
 
-// Write a function that takes an integer a and returns true
-// if this ingeger is a cube of some integer number.
-// Note: you may assume the input is always valid.
+// You have been tasked to write a function that receives
+// a hexadecimal number as a string and counts the number of hexadecimal
+// digits that are primes (prime number, or a prime, is a natural number
+// greater than 1 that is not a product of two smaller natural numbers).
+// Hexadecimal digits are 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F.
+// Prime numbers are 2, 3, 5, 7, 11, 13, 17,...
+// So you have to determine a number of the following digits: 2, 3, 5, 7,
+// B (=decimal 11), D (=decimal 13).
+// Note: you may assume the input is always correct or empty string,
+// and symbols A,B,C,D,E,F are always uppercase.
 // Examples:
-// Iscube(1) ==> true
-// Iscube(2) ==> false
-// Iscube(-1) ==> true
-// Iscube(64) ==> true
-// Iscube(0) ==> true
-// Iscube(180) ==> false
-func Iscube(a int) bool {
+// For num = "AB" the output should be 1.
+// For num = "1077E" the output should be 2.
+// For num = "ABED1A33" the output should be 4.
+// For num = "123456789ABCDEF0" the output should be 6.
+// For num = "2020" the output should be 2.
+func HexKey(num string) int {
 
-    abs := math.Abs(float64(a))
-	return int(math.Round(math.Pow(abs, 1.0/3.0))) == int(abs)
+    primes := map[int32]interface{}{'2': nil, '3': nil, '5': nil, '7': nil, 'B': nil, 'D': nil}
+	total := 1
+	for _, c := range num {
+		if _, ok := primes[c]; ok {
+			total++
+		}
+	}
+	return total
 }
 
-func ExampleTestIscube(t *testing.T) {
+func ExampleTestHexKey(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(true, Iscube(1))
-    assert.Equal(false, Iscube(2))
-    assert.Equal(true, Iscube(-1))
-    assert.Equal(true, Iscube(64))
-    assert.Equal(false, Iscube(180))
-    assert.Equal(true, Iscube(0))
+    assert.Equal(1, HexKey("AB"))
+    assert.Equal(2, HexKey("1077E"))
+    assert.Equal(4, HexKey("ABED1A33"))
+    assert.Equal(2, HexKey("2020"))
+    assert.Equal(6, HexKey("123456789ABCDEF0"))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestIscube(t)
+    ExampleTestHexKey(t)
 }
