ba876005fa1b9907e869b2a8fed3ba8b9e0974f1
swe-bench <>
2024-06-12 16:19:44 -0400

    Task instance Go/105

--- mymain.go
+++ mymain.go
@@ -6,46 +6,60 @@ import (
 )
 import (
     "sort"
-    "strconv"
 )
 
-// Given a list of positive integers x. return a sorted list of all
-// elements that hasn't any even digit.
-// 
-// Note: Returned list should be sorted in increasing order.
+// Given an array of integers, sort the integers that are between 1 and 9 inclusive,
+// reverse the resulting array, and then replace each digit by its corresponding name from
+// "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine".
 // 
 // For example:
-// >>> UniqueDigits([15, 33, 1422, 1])
-// [1, 15, 33]
-// >>> UniqueDigits([152, 323, 1422, 10])
-// []
-func UniqueDigits(x []int) []int {
+// arr = [2, 1, 1, 4, 5, 8, 2, 3]
+// -> sort arr -> [1, 1, 2, 2, 3, 4, 5, 8]
+// -> reverse arr -> [8, 5, 4, 3, 2, 2, 1, 1]
+// return ["Eight", "Five", "Four", "Three", "Two", "Two", "One", "One"]
+// 
+// If the array is empty, return an empty array:
+// arr = []
+// return []
+// 
+// If the array has any strange number ignore it:
+// arr = [1, -1 , 55]
+// -> sort arr -> [-1, 1, 55]
+// -> reverse arr -> [55, 1, -1]
+// return = ['One']
+func ByLength(arr []int)[]string {
 
-    odd_digit_elements := make([]int, 0)
-    OUTER:
-    for _, i := range x {
-        for _, c := range strconv.Itoa(i) {
-            if (c - '0') % 2 == 0 {
-                continue OUTER
-            }
-        }
-            odd_digit_elements = append(odd_digit_elements, i)
-            odd_digit_elements = append(odd_digit_elements, 1)
+    dic := map[int]string{
+        1: "One",
+        2: "Two",
+        3: "Three",
+        4: "Four",
+        5: "Five",
+        6: "Six",
+        7: "Seven",
+        8: "Eight",
+        9: "Nine",
     }
-    sort.Slice(odd_digit_elements, func(i, j int) bool {
-        return odd_digit_elements[i] < odd_digit_elements[j]
-    })
-    return odd_digit_elements
+    new_arr := make([]string, 0)
+    for _, item := range arr {
+        if v, ok := dic[item]; ok {
+            new_arr = append(new_arr, v)
+        }
+    }
+    return new_arr
 }
 
-func ExampleTestUniqueDigits(t *testing.T) {
+func ExampleTestByLength(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([]int{1, 15, 33}, UniqueDigits([]int{15, 33, 1422, 1}))
-    assert.Equal([]int{}, UniqueDigits([]int{152, 323, 1422, 10}))
+    assert.Equal([]string{"Eight", "Five", "Four", "Three", "Two", "Two", "One", "One"}, ByLength([]int{2, 1, 1, 4, 5, 8, 2, 3}))
+    assert.Equal([]string{}, ByLength([]int{}))
+    assert.Equal([]string{"One"}, ByLength([]int{1, -1 , 55}))
+    assert.Equal([]string{"Three", "Two", "One"}, ByLength([]int{1, -1, 3, 2}))
+    assert.Equal([]string{"Nine", "Eight", "Four"}, ByLength([]int{9, 4, 8}))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestUniqueDigits(t)
+    ExampleTestByLength(t)
 }
