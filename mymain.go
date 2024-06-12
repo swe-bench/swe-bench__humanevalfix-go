0564ed585835bd24faf0a56940b9566c956d7f12
swe-bench <>
2024-06-12 16:19:41 -0400

    Task instance Go/97

--- mymain.go
+++ mymain.go
@@ -4,46 +4,33 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
+import (
+    "math"
+)
 
-// Implement a function that takes an non-negative integer and returns an array of the first n
-// integers that are prime numbers and less than n.
-// for example:
-// CountUpTo(5) => [2,3]
-// CountUpTo(11) => [2,3,5,7]
-// CountUpTo(0) => []
-// CountUpTo(20) => [2,3,5,7,11,13,17,19]
-// CountUpTo(1) => []
-// CountUpTo(18) => [2,3,5,7,11,13,17]
-func CountUpTo(n int) []int {
+// Complete the function that takes two integers and returns
+// the product of their unit digits.
+// Assume the input is always valid.
+// Examples:
+// Multiply(148, 412) should return 16.
+// Multiply(19, 28) should return 72.
+// Multiply(2020, 1851) should return 0.
+// Multiply(14,-15) should return 20.
+func Multiply(a, b int) int {
 
-    primes := make([]int, 0)
-    for i := 2; i < n; i++ {
-        is_prime := true
-        for j := 2; j < i; j++ {
-            if j%i == 0 {
-                is_prime = false
-                break
-            }
-        }
-        if is_prime {
-            primes = append(primes, i)
-        }
-    }
-    return primes
+    return int(math.Abs(float64(a%10)) * math.Abs(float64(b%10)) * float64(a) * float64(b))
 }
 
-func ExampleTestCountUpTo(t *testing.T) {
+func ExampleTestMultiply(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([]int{2, 3}, CountUpTo(5))
-    assert.Equal([]int{2, 3, 5, 7}, CountUpTo(11))
-    assert.Equal([]int{}, CountUpTo(0))
-    assert.Equal([]int{2, 3, 5, 7, 11, 13, 17, 19}, CountUpTo(20))
-    assert.Equal([]int{}, CountUpTo(1))
-    assert.Equal([]int{2, 3, 5, 7, 11, 13, 17}, CountUpTo(18))
+    assert.Equal(16, Multiply(148, 412))
+    assert.Equal(72, Multiply(19, 28))
+    assert.Equal(0, Multiply(2020, 1851))
+    assert.Equal(20, Multiply(14, -15))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestCountUpTo(t)
+    ExampleTestMultiply(t)
 }
