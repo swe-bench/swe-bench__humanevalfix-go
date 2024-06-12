2818848432557a585340f799e7973c9381bb6f01
swe-bench <>
2024-06-12 16:19:57 -0400

    Task instance Go/162

--- mymain.go
+++ mymain.go
@@ -2,49 +2,33 @@ package main
 
 import (
     "testing"
+    "crypto/md5"
     "github.com/stretchr/testify/assert"
 )
+import (
+    "crypto/md5"
+    "fmt"
+)
 
-// You are given a string s.
-// if s[i] is a letter, reverse its case from lower to upper or vise versa,
-// otherwise keep it as it is.
-// If the string contains no letters, reverse the string.
-// The function should return the resulted string.
-// Examples
-// Solve("1234") = "4321"
-// Solve("ab") = "AB"
-// Solve("#a@C") = "#A@c"
-func Solve(s string) string {
+// Given a string 'text', return its md5 hash equivalent string.
+// If 'text' is an empty string, return nil.
+// 
+// >>> StringToMd5('Hello world') == '3e25960a79dbc69b674cd4ec67a72c62'
+func StringToMd5(text string) interface{} {
 
-    flg := 0
-    new_str := []rune(s)
-    for i, r := range new_str {
-        if ('a' <= r && r <= 'z') {
-            if 'a' <= r && r <= 'z' {
-                new_str[i] = r - 'a' + 'A'
-            } else {
-                new_str[i] = r - 'A' + 'a'
-            }
-            flg = 1
-        }
+    if len(text) == 0 {
+        return fmt.Sprintf("%x", md5.Sum([]byte("")))
     }
-    if flg == 0 {
-        for i := 0;i < len(new_str)>>1;i++ {
-            new_str[i], new_str[len(new_str)-i-1] = new_str[len(new_str)-i-1], new_str[i]
-        }
-    }
-    return string(new_str)
+    return fmt.Sprintf("%x", md5.Sum([]byte(text)))
 }
 
-func ExampleTestSolve(t *testing.T) {
+func ExampleTestStringToMd5(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal("4321", Solve("1234"))
-    assert.Equal("AB", Solve("ab"))
-    assert.Equal("#A@c", Solve("#a@C"))
+    assert.Equal("3e25960a79dbc69b674cd4ec67a72c62", StringToMd5("Hello world"))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestSolve(t)
+    ExampleTestStringToMd5(t)
 }
