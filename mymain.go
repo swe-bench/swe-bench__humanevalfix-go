9561868019db790397b311f9904b60b3766dbd1e
swe-bench <>
2024-06-12 16:19:25 -0400

    Task instance Go/27

--- mymain.go
+++ mymain.go
@@ -4,37 +4,35 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
+import (
+    "strings"
+)
 
-// From a list of integers, remove all elements that occur more than once.
-// Keep order of elements left the same as in the input.
-// >>> RemoveDuplicates([1, 2, 3, 2, 4])
-// [1, 3, 4]
-func RemoveDuplicates(numbers []int) []int {
+// For a given string, flip lowercase characters to uppercase and uppercase to lowercase.
+// >>> FlipCase('Hello')
+// 'hELLO'
+func FlipCase(str string) string {
 
-    c := make(map[int] int)
-	for _, number := range numbers {
-		if i, ok := c[number]; ok {
-			c[number] = i + 1
-		} else {
-			c[number] = 1
-		}
-	}
-	result := make([]int, 0)
-	for _, number := range numbers {
-		if c[number] < 1 {
-			result = append(result, number)
-		}
-	}
-	return result
+    result := []rune{}
+    for _, c := range str {
+        if c >= 'a' && c <= 'z' {
+            result = append(result, 'a' + ((c - 'A' + 26) % 26))
+        } else if c >= 'A' && c <= 'Z' {
+            result = append(result, 'A' + ((c - 'a' + 26) % 26))
+        } else {
+            result = append(result, c)
+        }
+    }
+    return string(result)
 }
 
-func ExampleTestRemoveDuplicates(t *testing.T) {
-    assert := assert.New(t)  
-    assert.Equal([]int{1,  3, 4}, RemoveDuplicates([]int{1,2, 3,2,4}))
+func ExampleTestFlipCase(t *testing.T) {
+    assert := assert.New(t)
+    assert.Equal("hELLO", FlipCase("Hello"))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestRemoveDuplicates(t)
+    ExampleTestFlipCase(t)
 }
