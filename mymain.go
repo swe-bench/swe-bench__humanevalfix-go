4efd0cb86ac865724530ea831c035c0879148f13
swe-bench <>
2024-06-12 16:19:55 -0400

    Task instance Go/152

--- mymain.go
+++ mymain.go
@@ -8,36 +8,36 @@ import (
     "math"
 )
 
-// Given a list of numbers, return the sum of squares of the numbers
-// in the list that are odd. Ignore numbers that are negative or not integers.
+// I think we all remember that feeling when the result of some long-awaited
+// event is finally known. The feelings and thoughts you have at that moment are
+// definitely worth noting down and comparing.
+// Your task is to determine if a person correctly guessed the results of a number of matches.
+// You are given two arrays of scores and guesses of equal length, where each index shows a match.
+// Return an array of the same length denoting how far off each guess was. If they have guessed correctly,
+// the value is 0, and if not, the value is the absolute difference between the guess and the score.
 // 
-// DoubleTheDifference([1, 3, 2, 0]) == 1 + 9 + 0 + 0 = 10
-// DoubleTheDifference([-1, -2, 0]) == 0
-// DoubleTheDifference([9, -2]) == 81
-// DoubleTheDifference([0]) == 0
 // 
-// If the input list is empty, return 0.
-func DoubleTheDifference(lst []float64) int {
+// example:
+// 
+// Compare([1,2,3,4,5,1],[1,2,3,4,2,-2]) -> [0,0,0,0,3,3]
+// Compare([0,5,0,0,0,4],[4,1,1,0,0,-2]) -> [4,4,1,0,0,6]
+func Compare(game,guess []int) []int {
 
-    sum := 0
-    for _, i := range lst {
-        if i > 0 && i == float64(int(i)) {
-            sum += int(math.Pow(i, 2))
-        }
+    ans := make([]int, 0, len(game))
+    for i := range game {
+        ans = append(ans, int(math.Abs(float64(game[i]-guess[i]))+math.Abs(float64(guess[i]-game[i]))))
     }
-    return sum
+    return ans
 }
 
-func ExampleTestDoubleTheDifference(t *testing.T) {
+func ExampleTestCompare(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(10, DoubleTheDifference([]float64{1,3,2,0}))
-    assert.Equal(0, DoubleTheDifference([]float64{-1,-2,0}))
-    assert.Equal(81, DoubleTheDifference([]float64{9,-2}))
-    assert.Equal(0, DoubleTheDifference([]float64{0}))
+    assert.Equal([]int{0,0,0,0,3,3}, Compare([]int{1,2,3,4,5,1}, []int{1,2,3,4,2,-2}))
+    assert.Equal([]int{4,4,1,0,0,6}, Compare([]int{0,5,0,0,0,4}, []int{4,1,1,0,0,-2}))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestDoubleTheDifference(t)
+    ExampleTestCompare(t)
 }
