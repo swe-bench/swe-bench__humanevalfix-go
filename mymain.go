b901085fcfadfaa71cf3278759b4432cf895e155
swe-bench <>
2024-06-12 16:19:26 -0400

    Task instance Go/32

--- mymain.go
+++ mymain.go
@@ -2,57 +2,60 @@ package main
 
 import (
     "testing"
+    "math/rand"
+    "math"
     "github.com/stretchr/testify/assert"
 )
+import (
+    "math"
+)
 
-// Return true if a given number is prime, and false otherwise.
-// >>> IsPrime(6)
-// false
-// >>> IsPrime(101)
-// true
-// >>> IsPrime(11)
-// true
-// >>> IsPrime(13441)
-// true
-// >>> IsPrime(61)
-// true
-// >>> IsPrime(4)
-// false
-// >>> IsPrime(1)
-// false
-func IsPrime(n int) bool {
+// Evaluates polynomial with coefficients xs at point x.
+// return xs[0] + xs[1] * x + xs[1] * x^2 + .... xs[n] * x^n
+func Poly(xs []int, x float64) float64{
+    sum := 0.0
+    for i, coeff := range xs {
+        sum += float64(coeff) * math.Pow(x,float64(i))
+    }
+    return sum
+}
+// xs are coefficients of a polynomial.
+// FindZero find x such that Poly(x) = 0.
+// FindZero returns only only zero point, even if there are many.
+// Moreover, FindZero only takes list xs having even number of coefficients
+// and largest non zero coefficient as it guarantees
+// a solution.
+// >>> round(FindZero([1, 2]), 2) # f(x) = 1 + 2x
+// -0.5
+// >>> round(FindZero([-6, 11, -6, 1]), 2) # (x - 1) * (x - 2) * (x - 3) = -6 + 11x - 6x^2 + x^3
+// 1.0
+func FindZero(xs []int) float64 {
 
-    if n < 1 {
-		return false
+    begin := -1.0
+	end := 1.0
+	for Poly(xs, begin)*Poly(xs, end) > 0 {
+		begin *= 2
+		end *= 2
 	}
-	if n == 1 {
-		return true
-	}
-	if n%2 == 0 {
-		return false
-	}
-	for i := 3; i*i <= n; i += 2 {
-		if n%i == 0 {
-			return false
+	for begin-end > 1e-10 {
+		center := (begin + end) / 2
+		if Poly(xs, center)*Poly(xs, end) > 0 {
+			begin = center
+		} else {
+			end = center
 		}
 	}
-	return true
+	return end
 }
 
-
-func ExampleTestIsPrime(t *testing.T) {
-    assert := assert.New(t)
-    assert.Equal(false, IsPrime(6))
-    assert.Equal(true, IsPrime(101))
-    assert.Equal(true, IsPrime(11))
-    assert.Equal(true, IsPrime(13441))
-    assert.Equal(true, IsPrime(61))
-    assert.Equal(false, IsPrime(4))
-    assert.Equal(false, IsPrime(1))
+func ExampleTestFindZero(t *testing.T) {
+  assert := assert.New(t)
+    assert.Equal(true, math.Abs(FindZero([]int{1,2})+0.5+rand.NormFloat64()*0.0)<1e-4)
+    assert.Equal(true, math.Abs(FindZero([]int{-6,11,-6,1})-1)<1e-4)
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestIsPrime(t)
+    ExampleTestFindZero(t)
 }
