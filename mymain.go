edfd6f2d3c7b12758a9277688d3c689ac9dd5d5a
swe-bench <>
2024-06-12 16:19:57 -0400

    Task instance Go/159

--- mymain.go
+++ mymain.go
@@ -4,46 +4,51 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
-import (
-    "sort"
-)
 
-// Write a function that accepts a list of strings.
-// The list contains different words. Return the word with maximum number
-// of unique characters. If multiple strings have maximum number of unique
-// characters, return the one which comes first in lexicographical order.
+// You're a hungry rabbit, and you already have Eaten a certain number of carrots,
+// but now you need to Eat more carrots to complete the day's meals.
+// you should return an array of [ total number of Eaten carrots after your meals,
+// the number of carrots left after your meals ]
+// if there are not enough remaining carrots, you will Eat all remaining carrots, but will still be hungry.
 // 
-// FindMax(["name", "of", "string"]) == "string"
-// FindMax(["name", "enam", "game"]) == "enam"
-// FindMax(["aaaaaaa", "bb" ,"cc"]) == ""aaaaaaa"
-func FindMax(words []string) string {
+// Example:
+// * Eat(5, 6, 10) -> [11, 4]
+// * Eat(4, 8, 9) -> [12, 1]
+// * Eat(1, 10, 10) -> [11, 0]
+// * Eat(2, 11, 5) -> [7, 0]
+// 
+// Variables:
+// @number : integer
+// the number of carrots that you have Eaten.
+// @need : integer
+// the number of carrots that you need to Eat.
+// @remaining : integer
+// the number of remaining carrots thet exist in stock
+// 
+// Constrain:
+// * 0 <= number <= 1000
+// * 0 <= need <= 1000
+// * 0 <= remaining <= 1000
+// 
+// Have fun :)
+func Eat(number, need, remaining int) []int {
 
-    key := func (word string) (int, string) {
-        set := make(map[rune]struct{})
-        for _, r := range word {
-            set[r] = struct{}{}
-        }
-        return -len(set), word
+    if(need <= remaining) {
+        return []int{ number + need , number+remaining-need }
     }
-    sort.SliceStable(words, func(i, j int) bool {
-        ia, ib := key(words[i])
-        ja, jb := key(words[j])
-        if ia == ja {
-            return ib < jb
-        }
-    })
-    return words[0]
+    return []int{ number + need + remaining , 0}
 }
 
-func ExampleTestFindMax(t *testing.T) {
+func ExampleTestEat(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal("string", FindMax([]string{"name", "of", "string"}))
-    assert.Equal("enam", FindMax([]string{"name", "enam", "game"}))
-    assert.Equal("aaaaaaa", FindMax([]string{"aaaaaaa", "bb", "cc"}))
+    assert.Equal([]int{11, 4}, Eat(5, 6, 10))
+    assert.Equal([]int{12, 1}, Eat(4, 8, 9))
+    assert.Equal([]int{11, 0}, Eat(1, 10, 10))
+    assert.Equal([]int{7, 0}, Eat(2, 11, 5))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestFindMax(t)
+    ExampleTestEat(t)
 }
