2335ae562af69da283da1cfb9f89093a93f46122
swe-bench <>
2024-06-12 16:19:22 -0400

    Task instance Go/18

--- mymain.go
+++ mymain.go
@@ -5,38 +5,34 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
-// Input to this function is a string representing musical notes in a special ASCII format.
-// Your task is to parse this string and return list of integers corresponding to how many beats does each
-// not last.
-// 
-// Here is a legend:
-// 'o' - whole note, lasts four beats
-// 'o|' - half note, lasts two beats
-// '.|' - quater note, lasts one beat
-// 
-// >>> ParseMusic('o o| .| o| o| .| .| .| .| o o')
-// [4, 2, 1, 2, 2, 1, 1, 1, 1, 4, 4]
-func ParseMusic(music_string string) []int{
+// Find how many times a given substring can be found in the original string. Count overlaping cases.
+// >>> HowManyTimes('', 'a')
+// 0
+// >>> HowManyTimes('aaa', 'a')
+// 3
+// >>> HowManyTimes('aaaa', 'aa')
+// 3
+func HowManyTimes(str string,substring string) int{
 
-    note_map := map[string]int{"o": 3, "o|": 2, ".|": 1}
-	split := strings.Split(music_string, " ")
-	result := make([]int, 0)
-	for _, x := range split {
-		if i, ok := note_map[x]; ok {
-			result = append(result, i)
+    times := 0
+	for i := 0; i < (len(str) - len(substring)); i++ {
+		if str[i:i+len(substring)] == substring {
+			times += 1
 		}
 	}
-	return result
+	return times
 }
 
 
-func ExampleTestParseMusic(t *testing.T) {
+func ExampleTestHowManyTimes(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([]int{4, 2, 1, 2, 2, 1, 1, 1, 1, 4, 4}, ParseMusic("o o| .| o| o| .| .| .| .| o o"))
+    assert.Equal(0, HowManyTimes("", "a"))
+    assert.Equal(3, HowManyTimes("aaa", "a"))
+    assert.Equal(3, HowManyTimes("aaaa", "aa"))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestParseMusic(t)
+    ExampleTestHowManyTimes(t)
 }
