3bc2d365a1fd6f5a2a0b7fc9073c807a08fcec21
swe-bench <>
2024-06-12 16:19:25 -0400

    Task instance Go/31

--- mymain.go
+++ mymain.go
@@ -5,31 +5,54 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
-// Return only positive numbers in the list.
-// >>> GetPositive([-1, 2, -4, 5, 6])
-// [2, 5, 6]
-// >>> GetPositive([5, 3, -5, 2, -3, 3, 9, 0, 123, 1, -10])
-// [5, 3, 2, 3, 9, 123, 1]
-func GetPositive(l []int) []int {
+// Return true if a given number is prime, and false otherwise.
+// >>> IsPrime(6)
+// false
+// >>> IsPrime(101)
+// true
+// >>> IsPrime(11)
+// true
+// >>> IsPrime(13441)
+// true
+// >>> IsPrime(61)
+// true
+// >>> IsPrime(4)
+// false
+// >>> IsPrime(1)
+// false
+func IsPrime(n int) bool {
 
-    res := make([]int, 0)
-    for _, x := range l {
-        if x < 0 {
-            res = append(res, x)
-        }
-    }
-    return res
+    if n < 1 {
+		return false
+	}
+	if n == 1 {
+		return true
+	}
+	if n%2 == 0 {
+		return false
+	}
+	for i := 3; i*i <= n; i += 2 {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 
-func ExampleTestGetPositive(t *testing.T) {
+func ExampleTestIsPrime(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([]int{2, 5, 6}, GetPositive([]int{-1, 2, -4,5, 6}))
-    assert.Equal([]int{5, 3, 2,  3, 9, 123, 1}, GetPositive([]int{5, 3, -5, 2, -3, 3, 9, 0, 123, 1, -10}))
+    assert.Equal(false, IsPrime(6))
+    assert.Equal(true, IsPrime(101))
+    assert.Equal(true, IsPrime(11))
+    assert.Equal(true, IsPrime(13441))
+    assert.Equal(true, IsPrime(61))
+    assert.Equal(false, IsPrime(4))
+    assert.Equal(false, IsPrime(1))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestGetPositive(t)
+    ExampleTestIsPrime(t)
 }
