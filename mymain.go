a1b85fbd3b75e0d9f6baec8c070e4070231e5d26
swe-bench <>
2024-06-12 16:19:57 -0400

    Task instance Go/160

--- mymain.go
+++ mymain.go
@@ -1,54 +1,64 @@
 package main
 
 import (
-    "testing"
-    "github.com/stretchr/testify/assert"
+    "math"
 )
 
-// You're a hungry rabbit, and you already have Eaten a certain number of carrots,
-// but now you need to Eat more carrots to complete the day's meals.
-// you should return an array of [ total number of Eaten carrots after your meals,
-// the number of carrots left after your meals ]
-// if there are not enough remaining carrots, you will Eat all remaining carrots, but will still be hungry.
+// Given two lists operator, and operand. The first list has basic algebra operations, and
+// the second list is a list of integers. Use the two given lists to build the algebric
+// expression and return the evaluation of this expression.
+// 
+// The basic algebra operations:
+// Addition ( + )
+// Subtraction ( - )
+// Multiplication ( * )
+// Floor division ( // )
+// Exponentiation ( ** )
 // 
 // Example:
-// * Eat(5, 6, 10) -> [11, 4]
-// * Eat(4, 8, 9) -> [12, 1]
-// * Eat(1, 10, 10) -> [11, 0]
-// * Eat(2, 11, 5) -> [7, 0]
+// operator['+', '*', '-']
+// array = [2, 3, 4, 5]
+// result = 2 + 3 * 4 - 5
+// => result = 9
 // 
-// Variables:
-// @number : integer
-// the number of carrots that you have Eaten.
-// @need : integer
-// the number of carrots that you need to Eat.
-// @remaining : integer
-// the number of remaining carrots thet exist in stock
-// 
-// Constrain:
-// * 0 <= number <= 1000
-// * 0 <= need <= 1000
-// * 0 <= remaining <= 1000
-// 
-// Have fun :)
-func Eat(number, need, remaining int) []int {
+// Note:
+// The length of operator list is equal to the length of operand list minus one.
+// Operand is a list of of non-negative integers.
+// Operator list has at least one operator, and operand list has at least two operands.
+func DoAlgebra(operator []string, operand []int) int {
 
-    if(need <= remaining) {
-        return []int{ number + need , number+remaining-need }
+    higher := func(a, b string) bool {
+        if b == "*" || b == "//" || b == "**" {
+            return false
+        }
+        if a == "*" || a == "//" || a == "**" {
+            return true
+        }
+        return false
     }
-    return []int{ number + need + remaining , 0}
-}
-
-func ExampleTestEat(t *testing.T) {
-    assert := assert.New(t)
-    assert.Equal([]int{11, 4}, Eat(5, 6, 10))
-    assert.Equal([]int{12, 1}, Eat(4, 8, 9))
-    assert.Equal([]int{11, 0}, Eat(1, 10, 10))
-    assert.Equal([]int{7, 0}, Eat(2, 11, 5))
-}
-
-func main() {
-    // Here you can call the test functions or any other code
-    t := &testing.T{}
-    ExampleTestEat(t)
+    for len(operand) > 1 {
+        pos := 0
+        sign := operator[0]
+        for i, str := range operator {
+            if higher(str, sign) {
+                sign = str
+                pos = i
+            }
+        }
+        switch sign {
+        case "+":
+            operand[pos] += operand[pos+1]
+        case "-":
+            operand[pos] -= operand[pos+1]
+        case "*":
+            operand[pos] *= operand[pos+1]
+        case "//":
+            operand[pos] /= operand[pos+1]
+        case "**":
+            operand[pos] = int(math.Pow(float64(operand[pos+1]), float64(operand[pos+1])))
+        }
+        operator = append(operator[:pos], operator[pos+1:]...)
+        operand = append(operand[:pos+1], operand[pos+2:]...)
+    }
+    return operand [0]
 }
