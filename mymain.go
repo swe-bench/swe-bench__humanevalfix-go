7903507df568b24bab00f1de2b44bd0c63410a42
swe-bench <>
2024-06-12 16:19:34 -0400

    Task instance Go/65

--- mymain.go
+++ mymain.go
@@ -4,41 +4,36 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
-import (
-    "strings"
-)
 
-// Write a function VowelsCount which takes a string representing
-// a word as input and returns the number of vowels in the string.
-// Vowels in this case are 'a', 'e', 'i', 'o', 'u'. Here, 'y' is also a
-// vowel, but only when it is at the end of the given word.
-// 
-// Example:
-// >>> VowelsCount("abcde")
-// 2
-// >>> VowelsCount("ACEDY")
-// 3
-func VowelsCount(s string) int {
+// Circular shift the digits of the integer x, shift the digits right by shift
+// and return the result as a string.
+// If shift > number of digits, return digits reversed.
+// >>> CircularShift(12, 1)
+// "21"
+// >>> CircularShift(12, 2)
+// "12"
+func CircularShift(x int,shift int) string {
 
-    s = strings.ToLower(s)
-	vowels := map[int32]interface{}{'a': nil, 'e': nil, 'i': nil, 'o': nil, 'u': nil, 'y': nil}
-	count := 0
-	for _, i := range s {
-		if _, ok := vowels[i]; ok {
-			count++
+    s := strconv.Itoa(x)
+	if shift > len(s) {
+		runes := make([]rune, 0)
+		for i := len(s)-1; i >= 0; i-- {
+			runes = append(runes, rune(s[i]))
 		}
+		return string(runes)
+	}else{
+		return s[:len(s)-shift]+s[len(s)-shift:]
 	}
-	return count
 }
 
-func ExampleTestVowelsCount(t *testing.T) {
+func ExampleTestCircularShift(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(2, VowelsCount("abcde"))
-    assert.Equal(3, VowelsCount("ACEDY"))
+    assert.Equal("12", CircularShift(12, 2))
+    assert.Equal("21", CircularShift(12, 1))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestVowelsCount(t)
+    ExampleTestCircularShift(t)
 }
