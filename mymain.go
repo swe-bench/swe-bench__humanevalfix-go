f81cc22ab282933ba31b3931d5173e7c19031990
swe-bench <>
2024-06-12 16:19:56 -0400

    Task instance Go/154

--- mymain.go
+++ mymain.go
@@ -4,56 +4,40 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
-import (
-    "math"
-)
 
-// You will be given the name of a class (a string) and a list of extensions.
-// The extensions are to be used to load additional classes to the class. The
-// strength of the extension is as follows: Let CAP be the number of the uppercase
-// letters in the extension's name, and let SM be the number of lowercase letters
-// in the extension's name, the strength is given by the fraction CAP - SM.
-// You should find the strongest extension and return a string in this
-// format: ClassName.StrongestExtensionName.
-// If there are two or more extensions with the same strength, you should
-// choose the one that comes first in the list.
-// For example, if you are given "Slices" as the class and a list of the
-// extensions: ['SErviNGSliCes', 'Cheese', 'StuFfed'] then you should
-// return 'Slices.SErviNGSliCes' since 'SErviNGSliCes' is the strongest extension
-// (its strength is -1).
-// Example:
-// for StrongestExtension('my_class', ['AA', 'Be', 'CC']) == 'my_class.AA'
-func StrongestExtension(class_name string, extensions []string) string {
+// You are given 2 words. You need to return true if the second word or any of its rotations is a substring in the first word
+// CycpatternCheck("abcd","abd") => false
+// CycpatternCheck("hello","ell") => true
+// CycpatternCheck("whassup","psus") => false
+// CycpatternCheck("abab","baa") => true
+// CycpatternCheck("efef","eeff") => false
+// CycpatternCheck("himenss","simen") => true
+func CycpatternCheck(a , b string) bool {
 
-    strong := extensions[0]
-    
-    my_val := math.MinInt
-    for _, s := range extensions {
-        cnt0, cnt1 := 0, 0
-        for _, c := range s {
-            switch {
-            case 'A' <= c && c <= 'Z':
-                cnt0++
-            case 'a' <= c && c <= 'z':
-                cnt1++
+    l := len(b)
+    pat := b + b
+    for i := 0;i < len(a) - l + 1; i++ {
+        for j := 0;j < len(b) - l + 1;j++ {
+            if a[i:i+l] == pat[j:j+l] {
+                return true
             }
         }
-        val := cnt0-cnt1
-        if val > my_val {
-            strong = s
-            my_val = val
-        }
     }
-    return class_name + strong
+    return false
 }
 
-func ExampleTestStrongestExtension(t *testing.T) {
+func ExampleTestCycpatternCheck(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal("my_class.AA", StrongestExtension("my_class", []string{"AA", "Be", "CC"}))
+    assert.Equal(false, CycpatternCheck("abcd", "abd"))
+    assert.Equal(true, CycpatternCheck("hello", "ell"))
+    assert.Equal(false, CycpatternCheck("whasup", "psus"))
+    assert.Equal(true, CycpatternCheck("abab", "baa"))
+    assert.Equal(false, CycpatternCheck("efef", "eeff"))
+    assert.Equal(true, CycpatternCheck("himenss", "simen"))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestStrongestExtension(t)
+    ExampleTestCycpatternCheck(t)
 }
