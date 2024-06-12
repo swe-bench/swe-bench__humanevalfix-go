f25cab6e12b78d3e2f565624ddd434b5f53e3ab3
swe-bench <>
2024-06-12 16:19:46 -0400

    Task instance Go/113

--- mymain.go
+++ mymain.go
@@ -5,45 +5,42 @@ import (
     "github.com/stretchr/testify/assert"
 )
 import (
-    "strings"
+    "fmt"
 )
 
-// Task
-// We are given two strings s and c, you have to deleted all the characters in s that are equal to any character in c
-// then check if the result string is palindrome.
-// A string is called palindrome if it reads the same backward as forward.
-// You should return a tuple containing the result string and true/false for the check.
-// Example
-// For s = "abcde", c = "ae", the result should be ('bcd',false)
-// For s = "abcdef", c = "b"  the result should be ('acdef',false)
-// For s = "abcdedcba", c = "ab", the result should be ('cdedc',true)
-func ReverseDelete(s,c string) [2]interface{} {
+// Given a list of strings, where each string consists of only digits, return a list.
+// Each element i of the output should be "the number of odd elements in the
+// string i of the input." where all the i's should be replaced by the number
+// of odd digits in the i'th string of the input.
+// 
+// >>> OddCount(['1234567'])
+// ["the number of odd elements 4n the str4ng 4 of the 4nput."]
+// >>> OddCount(['3',"11111111"])
+// ["the number of odd elements 1n the str1ng 1 of the 1nput.",
+// "the number of odd elements 8n the str8ng 8 of the 8nput."]
+func OddCount(lst []string) []string {
 
-    rs := make([]rune, 0)
-    for _, r := range s {
-        if !strings.ContainsRune(c, r) {
-            rs = append(rs, r)
+    res := make([]string, 0, len(lst))
+    for _, arr := range lst {
+        n := 0
+        for _, d := range arr {
+            if (d - '0') % 2 == 1 {
+                n++
+            }
         }
+        res = append(res, fmt.Sprintf("the number of odd elements %dn the str%dng %d of %d the %dnput.", n,n,n,n,n))
     }
-    t := false
-    for i := 0;i < len(rs)>>1;i++ {
-        if rs[i] != rs[len(rs)-i-1] {
-            t=true
-            break
-        }
-    }
-    return [2]interface{}{string(rs), t}
+    return res
 }
 
-func ExampleTestReverseDelete(t *testing.T) {
+func ExampleTestOddCount(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([2]interface{}{"bcd", false}, ReverseDelete("abcde","ae"))
-    assert.Equal([2]interface{}{"acdef", false}, ReverseDelete("abcdef", "b"))
-    assert.Equal([2]interface{}{"cdedc", true}, ReverseDelete("abcdedcba","ab"))
+    assert.Equal([]string{"the number of odd elements 4n the str4ng 4 of the 4nput."}, OddCount([]string{"1234567"}))
+    assert.Equal([]string{"the number of odd elements 1n the str1ng 1 of the 1nput.", "the number of odd elements 8n the str8ng 8 of the 8nput."}, OddCount([]string{"3", "11111111"}))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestReverseDelete(t)
+    ExampleTestOddCount(t)
 }
