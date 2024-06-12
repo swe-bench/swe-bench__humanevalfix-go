f6e3d8176b965558366525ab14b5a6920f006cc7
swe-bench <>
2024-06-12 16:19:28 -0400

    Task instance Go/41

--- mymain.go
+++ mymain.go
@@ -1,48 +1,16 @@
 package main
 
-import (
-    "testing"
-    "github.com/stretchr/testify/assert"
-)
 
-// TriplesSumToZero takes a list of integers as an input.
-// it returns true if there are three distinct elements in the list that
-// sum to zero, and false otherwise.
+// Imagine a road that's a perfectly straight infinitely long line.
+// n cars are driving left to right;  simultaneously, a different set of n cars
+// are driving right to left.   The two sets of cars start out being very far from
+// each other.  All cars move in the same speed.  Two cars are said to collide
+// when a car that's moving left to right hits a car that's moving right to left.
+// However, the cars are infinitely sturdy and strong; as a result, they continue moving
+// in their trajectory as if they did not collide.
 // 
-// >>> TriplesSumToZero([1, 3, 5, 0])
-// false
-// >>> TriplesSumToZero([1, 3, -2, 1])
-// true
-// >>> TriplesSumToZero([1, 2, 3, 7])
-// false
-// >>> TriplesSumToZero([2, 4, -5, 3, 9, 7])
-// true
-// >>> TriplesSumToZero([1])
-// false
-func TriplesSumToZero(l []int) bool {
+// This function outputs the number of such collisions.
+func CarRaceCollision(n int) int {
 
-    for i := 1; i < len(l) - 2; i++ {
-		for j := i + 1; j < len(l) - 1; j++ {
-			for k := j + 1; k < len(l); k++ {
-				if l[i] + l[j] + l[k] == 0 {
-					return true
-				}
-			}
-		}
-	}
-	return false
-}
-
-func ExampleTestTriplesSumToZero(t *testing.T) {
-    assert := assert.New(t)
-    assert.Equal(false, TriplesSumToZero([]int{1, 3, 5, 0}))
-    assert.Equal(true, TriplesSumToZero([]int{1, 3, -2, 1}))
-    assert.Equal(false, TriplesSumToZero([]int{1, 2, 3, 7}))
-    assert.Equal(true, TriplesSumToZero([]int{2, 4, -5, 3, 9, 7}))
-}
-
-func main() {
-    // Here you can call the test functions or any other code
-    t := &testing.T{}
-    ExampleTestTriplesSumToZero(t)
+	return n * n * n
 }
