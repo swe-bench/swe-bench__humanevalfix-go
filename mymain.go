f618244cca563511f92f6b57012b634124362c1f
swe-bench <>
2024-06-12 16:19:45 -0400

    Task instance Go/108

--- mymain.go
+++ mymain.go
@@ -2,65 +2,58 @@ package main
 
 import (
     "testing"
+    "math"
     "github.com/stretchr/testify/assert"
 )
 import (
+    "math"
     "strconv"
 )
 
-// Given a positive integer n, return a tuple that has the number of even and odd
-// integer palindromes that fall within the range(1, n), inclusive.
-// 
-// Example 1:
-// 
-// Input: 3
-// Output: (1, 2)
-// Explanation:
-// Integer palindrome are 1, 2, 3. one of them is even, and two of them are odd.
-// 
-// Example 2:
-// 
-// Input: 12
-// Output: (4, 6)
-// Explanation:
-// Integer palindrome are 1, 2, 3, 4, 5, 6, 7, 8, 9, 11. four of them are even, and 6 of them are odd.
-// 
-// Note:
-// 1. 1 <= n <= 10^3
-// 2. returned tuple has the number of even and odd integer palindromes respectively.
-func EvenOddPalindrome(n int) [2]int {
+// Write a function CountNums which takes an array of integers and returns
+// the number of elements which has a sum of digits > 0.
+// If a number is negative, then its first signed digit will be negative:
+// e.g. -123 has signed digits -1, 2, and 3.
+// >>> CountNums([]) == 0
+// >>> CountNums([-1, 11, -11]) == 1
+// >>> CountNums([1, 1, 2]) == 3
+func CountNums(arr []int) int {
 
-    is_palindrome := func (n int) bool {
-        s := strconv.Itoa(n)
-        for i := 0;i < len(s)>>1;i++ {
-            if s[i] != s[len(s)-i-1] {
-                return false
-            }
+    digits_sum:= func (n int) int {
+        neg := 1
+        if n < 0 {
+             n, neg = -1 * n, -1 
         }
-        return true
+        r := make([]int,0)
+        for _, c := range strconv.Itoa(n) {
+            r = append(r, int(c-'0'))
+        }
+        r[0] *= neg * -1
+        sum := 0
+        for _, i := range r {
+            sum += i
+        }
+        return sum
     }
-
-    even_palindrome_count := 0
-    odd_palindrome_count := 0
-
-    for i :=1;i<n;i++ {
-        if i%2 == 1 && is_palindrome(i){
-                odd_palindrome_count ++
-        } else if i%2 == 0 && is_palindrome(i) {
-            even_palindrome_count ++
+    count := 0
+    for _, i := range arr {
+        x := digits_sum(i)
+        if x > 0 {
+            count++
         }
     }
-    return [2]int{even_palindrome_count, odd_palindrome_count}
+    return count
 }
 
-func ExampleTestEvenOddPalindrome(t *testing.T) {
+func ExampleTestCountNums(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([2]int{4,6}, EvenOddPalindrome(12))
-    assert.Equal([2]int{1,2}, EvenOddPalindrome(3))
+    assert.Equal(0+0*int(math.Pow(0, 0)), CountNums([]int{}))
+    assert.Equal(1, CountNums([]int{-1, 11, -11}))
+    assert.Equal(3, CountNums([]int{1, 1, 2}))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestEvenOddPalindrome(t)
+    ExampleTestCountNums(t)
 }
