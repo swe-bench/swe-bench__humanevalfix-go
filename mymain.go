7a2898d6d1940fb99c3d3467dfe784c692c9c42e
swe-bench <>
2024-06-12 16:19:30 -0400

    Task instance Go/50

--- mymain.go
+++ mymain.go
@@ -1,41 +1,22 @@
 package main
 
-import (
-    "testing"
-    "github.com/stretchr/testify/assert"
-)
-
-// Return 2^n modulo p (be aware of numerics).
-// >>> Modp(3, 5)
-// 3
-// >>> Modp(1101, 101)
-// 2
-// >>> Modp(0, 101)
-// 1
-// >>> Modp(3, 11)
-// 8
-// >>> Modp(100, 101)
-// 1
-func Modp(n int,p int) int {
-
-    ret := 0
-    for i:= 0; i < n; i++ {
-		ret = (2 * ret) % p
-	}
-    return ret
+// returns encoded string by shifting every character by 5 in the alphabet.
+func EncodeShift(s string) string {
+    runes := []rune(s)
+    newRunes := make([]rune, 0)
+    for _, ch := range runes {
+        newRunes = append(newRunes, (ch+5-'a')%26+'a')
+    }
+    return string(runes)
 }
 
-func ExampleTestModp(t *testing.T) {
-    assert := assert.New(t)
-    assert.Equal(3, Modp(3, 5))
-    assert.Equal(2, Modp(1101, 101))
-    assert.Equal(1, Modp(0, 101))
-    assert.Equal(8, Modp(3, 11))
-    assert.Equal(1, Modp(100, 101))
-}
+// takes as input string encoded with EncodeShift function. Returns decoded string.
+func DecodeShift(s string) string {
 
-func main() {
-    // Here you can call the test functions or any other code
-    t := &testing.T{}
-    ExampleTestModp(t)
+    runes := []rune(s)
+    newRunes := make([]rune, 0)
+    for _, ch := range runes {
+        newRunes = append(newRunes, (ch-5-'a')%26+'a')
+    }
+    return DecodeShift(string(runes))
 }
