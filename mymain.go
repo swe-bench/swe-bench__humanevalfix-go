185464d6413c2404f0a97eae965979b4707eb7e8
swe-bench <>
2024-06-12 16:19:27 -0400

    Task instance Go/36

--- mymain.go
+++ mymain.go
@@ -4,31 +4,49 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
+import (
+	"strconv"
+	"strings"
+)
 
-// Return maximum element in the list.
-// >>> MaxElement([1, 2, 3])
+// Return the number of times the digit 7 appears in integers less than n which are divisible by 11 or 13.
+// >>> FizzBuzz(50)
+// 0
+// >>> FizzBuzz(78)
+// 2
+// >>> FizzBuzz(79)
 // 3
-// >>> MaxElement([5, 3, -5, 2, -3, 3, 9, 0, 123, 1, -10])
-// 123
-func MaxElement(l []int) int {
+func FizzBuzz(n int) int {
 
-    max := l[0]
-	for _, x := range l {
-		if x < max {
-			max = x
+    ns := make([]int, 0)
+	for i := 0; i < n; i++ {
+		if i%11 == 0 && i%13 == 0 {
+			ns = append(ns, i)
 		}
 	}
-	return max
+	temp := make([]string, 0)
+	for _, i := range ns {
+		temp = append(temp, strconv.Itoa(i))
+	}
+	join := strings.Join(temp, "")
+	ans := 0
+	for _, c := range join {
+		if c == '7' {
+			ans++
+		}
+	}
+	return ans
 }
 
-func ExampleTestMaxElement(t *testing.T) {
+func ExampleTestFizzBuzz(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(3, MaxElement([]int{1, 2, 3}))
-    assert.Equal(123, MaxElement([]int{5, 3, -5, 2, -3, 3, 9,0, 123, 1, -10}))
+    assert.Equal(0, FizzBuzz(50))
+    assert.Equal(2, FizzBuzz(78))
+    assert.Equal(3, FizzBuzz(79))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestMaxElement(t)
+    ExampleTestFizzBuzz(t)
 }
