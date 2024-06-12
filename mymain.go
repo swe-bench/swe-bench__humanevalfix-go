00f6d374dbf5175c0147731b9ae6040b655f1627
swe-bench <>
2024-06-12 16:19:44 -0400

    Task instance Go/106

--- mymain.go
+++ mymain.go
@@ -4,62 +4,42 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
-import (
-    "sort"
-)
+ 
+// Implement the Function F that takes n as a parameter,
+// and returns a list of size n, such that the value of the element at index i is the factorial of i if i is even
+// or the sum of numbers from 1 to i otherwise.
+// i starts from 1.
+// the factorial of i is the multiplication of the numbers from 1 to i (1 * 2 * ... * i).
+// Example:
+// F(5) == [1, 2, 6, 24, 15]
+func F(n int) []int {
 
-// Given an array of integers, sort the integers that are between 1 and 9 inclusive,
-// reverse the resulting array, and then replace each digit by its corresponding name from
-// "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine".
-// 
-// For example:
-// arr = [2, 1, 1, 4, 5, 8, 2, 3]
-// -> sort arr -> [1, 1, 2, 2, 3, 4, 5, 8]
-// -> reverse arr -> [8, 5, 4, 3, 2, 2, 1, 1]
-// return ["Eight", "Five", "Four", "Three", "Two", "Two", "One", "One"]
-// 
-// If the array is empty, return an empty array:
-// arr = []
-// return []
-// 
-// If the array has any strange number ignore it:
-// arr = [1, -1 , 55]
-// -> sort arr -> [-1, 1, 55]
-// -> reverse arr -> [55, 1, -1]
-// return = ['One']
-func ByLength(arr []int)[]string {
-
-    dic := map[int]string{
-        1: "One",
-        2: "Two",
-        3: "Three",
-        4: "Four",
-        5: "Five",
-        6: "Six",
-        7: "Seven",
-        8: "Eight",
-        9: "Nine",
-    }
-    new_arr := make([]string, 0)
-    for _, item := range arr {
-        if v, ok := dic[item]; ok {
-            new_arr = append(new_arr, v)
+    ret := make([]int, 0, 5)
+    for i:=1;i<n+1;i++{
+        if i%2 == 0 {
+            x := 1
+            for j:=1;j<i+1;j++{
+                x*=j
+            }
+            ret = append(ret, x)
+        }else {
+            x := 0
+            for j:=1;j<i+1;j++{
+                x+=i
+            }
+            ret = append(ret, x)
         }
     }
-    return new_arr
+    return ret
 }
 
-func ExampleTestByLength(t *testing.T) {
+func ExampleTestF(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([]string{"Eight", "Five", "Four", "Three", "Two", "Two", "One", "One"}, ByLength([]int{2, 1, 1, 4, 5, 8, 2, 3}))
-    assert.Equal([]string{}, ByLength([]int{}))
-    assert.Equal([]string{"One"}, ByLength([]int{1, -1 , 55}))
-    assert.Equal([]string{"Three", "Two", "One"}, ByLength([]int{1, -1, 3, 2}))
-    assert.Equal([]string{"Nine", "Eight", "Four"}, ByLength([]int{9, 4, 8}))
+    assert.Equal([]int{1, 2, 6, 24, 15}, F(5))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestByLength(t)
+    ExampleTestF(t)
 }
