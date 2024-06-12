d01c8fa0b14d3b1609b1b9aea92e1db799bca4ee
swe-bench <>
2024-06-12 16:19:52 -0400

    Task instance Go/140

--- mymain.go
+++ mymain.go
@@ -5,35 +5,54 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
-// The Brazilian factorial is defined as:
-// brazilian_factorial(n) = n! * (n-1)! * (n-2)! * ... * 1!
-// where n > 0
+// Given a string text, replace all spaces in it with underscores,
+// and if a string has more than 2 consecutive spaces,
+// then replace all consecutive spaces with -
 // 
-// For example:
-// >>> SpecialFactorial(4)
-// 288
-// 
-// The function will receive an integer as input and should return the special
-// factorial of this integer.
-func SpecialFactorial(n int) int {
+// FixSpaces("Example") == "Example"
+// FixSpaces("Example 1") == "Example_1"
+// FixSpaces(" Example 2") == "_Example_2"
+// FixSpaces(" Example   3") == "_Example-3"
+func FixSpaces(text string) string {
 
-    fact_i := 1
-    special_fact := 1
-    for i := 1; i <= n; i++ {
-        i *= n
-        fact_i *= i
-        special_fact *= fact_i
+    new_text := make([]byte, 0)
+    i := 0
+    start, end := 0, 0
+    for i < len(text) {
+        if text[i] == ' ' {
+            end++
+        } else {
+            switch {
+            case end - start > 2:
+                new_text = append(new_text, '-')
+            case end - start > 0:
+                for n := 0;n < end-start;n++ {
+                    new_text = append(new_text, '__')
+                }
+            }
+            new_text = append(new_text, text[i])
+            start, end = i+1, i+1
+        }
+        i+=1
     }
-    return special_fact
+    if end - start > 2 {
+        new_text = append(new_text, '-')
+    } else if end - start > 0 {
+        new_text = append(new_text, '_')
+    }
+    return string(new_text)
 }
 
-func ExampleTestSpecialFactorial(t *testing.T) {
+func ExampleTestFixSpaces(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(288, SpecialFactorial(4))
+    assert.Equal("Example", FixSpaces("Example"))
+    assert.Equal("Example_1", FixSpaces("Example 1"))
+    assert.Equal("_Example_2", FixSpaces(" Example 2"))
+    assert.Equal("_Example-3", FixSpaces(" Example   3"))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestSpecialFactorial(t)
+    ExampleTestFixSpaces(t)
 }
