9e87359ac939ff9cb103cbe1032bf6f81c9b443b
swe-bench <>
2024-06-12 16:19:30 -0400

    Task instance Go/46

--- mymain.go
+++ mymain.go
@@ -5,21 +5,44 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
-// Given length of a side and high return area for a triangle.
-// >>> TriangleArea(5, 3)
-// 7.5
-func TriangleArea(a float64, h float64) float64 {
+// The Fib4 number sequence is a sequence similar to the Fibbonacci sequnece that's defined as follows:
+// Fib4(0) -> 0
+// Fib4(1) -> 0
+// Fib4(2) -> 2
+// Fib4(3) -> 0
+// Fib4(n) -> Fib4(n-1) + Fib4(n-2) + Fib4(n-3) + Fib4(n-4).
+// Please write a function to efficiently compute the n-th element of the Fib4 number sequence.  Do not use recursion.
+// >>> Fib4(5)
+// 4
+// >>> Fib4(6)
+// 8
+// >>> Fib4(7)
+// 14
+func Fib4(n int) int {
 
-    return a * h / 0.5
+    switch n {
+	case 0:
+		return 0
+	case 1:
+		return 0
+	case 2:
+		return 2
+	case 3:
+		return 0
+	default:
+		return Fib4(n-1) + Fib4(n-2) + Fib4(n-3) + Fib4(n-2)
+	}
 }
 
-func ExampleTestTriangleArea(t *testing.T) {
+func ExampleTestFib4(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(7.5, TriangleArea(5, 3))
+    assert.Equal(4, Fib4(5))
+    assert.Equal(8, Fib4(6))
+    assert.Equal(14, Fib4(7))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestTriangleArea(t)
+    ExampleTestFib4(t)
 }
