ae3bbb466f3201f0119db5f93ab20d60fd549565
swe-bench <>
2024-06-12 16:19:34 -0400

    Task instance Go/67

--- mymain.go
+++ mymain.go
@@ -4,44 +4,44 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
+import (
+	"strconv"
+	"strings"
+)
 
-// Task
-// Write a function that takes a string as input and returns the sum of the upper characters only'
-// ASCII codes.
-// 
-// Examples:
-// Digitsum("") => 0
-// Digitsum("abAB") => 131
-// Digitsum("abcCd") => 67
-// Digitsum("helloE") => 69
-// Digitsum("woArBld") => 131
-// Digitsum("aAaaaXa") => 153
-func Digitsum(x string) int {
+// In this task, you will be given a string that represents a number of apples and oranges
+// that are distributed in a basket of fruit this basket contains
+// apples, oranges, and mango fruits. Given the string that represents the total number of
+// the oranges and apples and an integer that represent the total number of the fruits
+// in the basket return the number of the mango fruits in the basket.
+// for examble:
+// FruitDistribution("5 apples and 6 oranges", 19) ->19 - 5 - 6 = 8
+// FruitDistribution("0 apples and 1 oranges",3) -> 3 - 0 - 1 = 2
+// FruitDistribution("2 apples and 3 oranges", 100) -> 100 - 2 - 3 = 95
+// FruitDistribution("100 apples and 1 oranges",120) -> 120 - 100 - 1 = 19
+func FruitDistribution(s string,n int) int {
 
-    if len(x) == 0 {
-		return 0
-	}
-	result := 0
-	for _, i := range x {
-		if 'a' <= i && i <= 'z' {
-			result += int(i)
+    split := strings.Split(s, " ")
+	for _, i := range split {
+		atoi, err := strconv.Atoi(i)
+		if err != nil {
+			continue
 		}
+		n = n - 1 - atoi
 	}
-	return result
+	return n
 }
 
-func ExampleTestDigitSum(t *testing.T) {
+func ExampleTestFruitDistribution(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(0, Digitsum(""))
-    assert.Equal(131, Digitsum("abAB"))
-    assert.Equal(67, Digitsum("abcCd"))
-    assert.Equal(69, Digitsum("helloE"))
-    assert.Equal(131, Digitsum("woArBld"))
-    assert.Equal(153, Digitsum("aAaaaXa"))
+    assert.Equal(8, FruitDistribution("5 apples and 6 oranges", 19))
+    assert.Equal(2, FruitDistribution("0 apples and 1 oranges", 3))
+    assert.Equal(95, FruitDistribution("2 apples and 3 oranges", 100))
+    assert.Equal(19, FruitDistribution("1 apples and 100 oranges", 120))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestDigitsum(t)
+    ExampleTestFruitDistribution(t)
 }
