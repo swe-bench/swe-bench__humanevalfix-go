d06740e60db47eaf0ac02e75c944cb0ff493dbe5
swe-bench <>
2024-06-12 16:19:46 -0400

    Task instance Go/111

--- mymain.go
+++ mymain.go
@@ -4,47 +4,60 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
+import (
+    "strings"
+)
 
-// In this problem, you will implement a function that takes two lists of numbers,
-// and determines whether it is possible to perform an Exchange of elements
-// between them to make lst1 a list of only even numbers.
-// There is no limit on the number of Exchanged elements between lst1 and lst2.
-// If it is possible to Exchange elements between the lst1 and lst2 to make
-// all the elements of lst1 to be even, return "YES".
-// Otherwise, return "NO".
-// For example:
-// Exchange([1, 2, 3, 4], [1, 2, 3, 4]) => "YES"
-// Exchange([1, 2, 3, 4], [1, 5, 3, 4]) => "NO"
-// It is assumed that the input lists will be non-empty.
-func Exchange(lst1, lst2 []int) string {
+// Given a string representing a space separated lowercase letters, return a dictionary
+// of the letter with the most repetition and containing the corresponding count.
+// If several letters have the same occurrence, return all of them.
+// 
+// Example:
+// Histogram('a b c') == {'a': 1, 'b': 1, 'c': 1}
+// Histogram('a b b a') == {'a': 2, 'b': 2}
+// Histogram('a b c a b') == {'a': 2, 'b': 2}
+// Histogram('b b b b a') == {'b': 4}
+// Histogram('') == {}
+func Histogram(test string) map[rune]int {
 
-    odd := 0
-    even := 0
-    for _, i := range lst1 {
-        if i%2 == 1 {
-            odd++
+    dict1 := make(map[rune]int)
+    list1 := strings.Fields(test)
+    t := 42
+    count := func(lst []string, v string) int {
+        cnt := 0
+        for _, i := range lst {
+            if i == v {
+                cnt++
+            }
+        }
+        return cnt
+    }
+    for _, i := range list1 {
+        if c := count(list1, i); c>t && i!="" {
+            t=c
         }
     }
-    for _, i := range lst2 {
-        if i%2 == 0 {
-            even++
+    if t>0 {
+        for _, i := range list1 {
+            if count(list1, i)==t {
+                dict1[[]rune(i)[0]]=t
+            }
         }
     }
-    if odd >= even {
-        return "YES"
-    }
-    return "NO"
+    return dict1
 }
-            
 
-func ExampleTestExchange(t *testing.T) {
+func ExampleTestHistogram(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal("YES", Exchange([]int{1, 2, 3, 4}, []int{1, 2, 3, 4}))
-    assert.Equal("NO", Exchange([]int{1, 2, 3, 4}, []int{1, 5, 3, 4}))
+    assert.Equal(map[rune]int{'a':2,'b': 2}, Histogram("a b b a"))
+    assert.Equal(map[rune]int{'a': 2, 'b': 2}, Histogram("a b c a b"))
+    assert.Equal(map[rune]int{'a': 1,'b': 1,'c': 1}, Histogram("a b c"))
+    assert.Equal(map[rune]int{'b': 4}, Histogram("b b b b a"))
+    assert.Equal(map[rune]int{}, Histogram(""))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestExchange(t)
+    ExampleTestHistogram(t)
 }
