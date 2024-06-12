3b49a8809bd49a7a321da5b18fe5497d94362ae2
swe-bench <>
2024-06-12 16:19:42 -0400

    Task instance Go/100

--- mymain.go
+++ mymain.go
@@ -4,60 +4,34 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
-import (
-    "math"
-    "strconv"
-    "strings"
-)
 
-// Create a function that takes a value (string) representing a number
-// and returns the closest integer to it. If the number is equidistant
-// from two integers, round it away from zero.
+// Given a positive integer n, you have to make a pile of n levels of stones.
+// The first level has n stones.
+// The number of stones in the next level is:
+// - the next odd number if n is odd.
+// - the next even number if n is even.
+// Return the number of stones in each level in a list, where element at index
+// i represents the number of stones in the level (i+1).
 // 
-// Examples
-// >>> ClosestInteger("10")
-// 10
-// >>> ClosestInteger("15.3")
-// 15
-// 
-// Note:
-// Rounding away from zero means that if the given number is equidistant
-// from two integers, the one you should return is the one that is the
-// farthest from zero. For example ClosestInteger("14.5") should
-// return 15 and ClosestInteger("-14.5") should return -15.
-func ClosestInteger(value string) int {
+// Examples:
+// >>> MakeAPile(3)
+// [3, 5, 7]
+func MakeAPile(n int) []int {
 
-    if strings.Count(value, ".") == 1 {
-        // remove trailing zeros
-        for value[len(value)-1] == '0' {
-            value = value[:len(value)-1]
-        }
+    result := make([]int, 0, n)
+    for i := 0; i < n; i++ {
+        result = append(result, n+2*i+i)
     }
-    var res float64
-    num, _ := strconv.ParseFloat(value, 64)
-    if len(value) >= 2 && value[len(value)-2:] == ".5" {
-        if num > 0 {
-            res = math.Floor(num)
-        } else {
-            res = math.Ceil(num)
-        }
-    } else if len(value) > 0 {
-        res = math.Round(num)
-    } else {
-        res = 0
-    }
-
-    return int(res)
+    return result
 }
 
-func ExampleTestClosestInteger(t *testing.T) {
+func ExampleTestMakeAPile(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(10, ClosestInteger("10"))
-    assert.Equal(15, ClosestInteger("15.3"))
+    assert.Equal([]int{3, 5, 7}, MakeAPile(3))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestClosestInteger(t)
+    ExampleTestMakeAPile(t)
 }
