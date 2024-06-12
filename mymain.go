c5d34e26616a952488b032f8f5dbaf2783389779
swe-bench <>
2024-06-12 16:19:20 -0400

    Task instance Go/7

--- mymain.go
+++ mymain.go
@@ -5,46 +5,33 @@ import (
     "github.com/stretchr/testify/assert"
 )
 import (
-    "math"
     "strings"
 )
 
-// Input to this function is a string represented multiple groups for nested parentheses separated by spaces.
-// For each of the group, output the deepest level of nesting of parentheses.
-// E.g. (()()) has maximum two levels of nesting while ((())) has three.
-// 
-// >>> ParseNestedParens('(()()) ((())) () ((())()())')
-// [2, 3, 1, 3]
-func ParseNestedParens(paren_string string) []int {
+// Filter an input list of strings only for ones that contain given substring
+// >>> FilterBySubstring([], 'a')
+// []
+// >>> FilterBySubstring(['abc', 'bacd', 'cde', 'array'], 'a')
+// ['abc', 'bacd', 'array']
+func FilterBySubstring(stringList []string, substring string) []string {
 
-    parse_paren_group := func(s string) int {
-        depth := 0
-        max_depth := 0
-        for _, c := range s {
-            if c == '(' {
-                depth += 1
-                max_depth = int(math.Max(float64(depth), float64(max_depth)))
-            } else {
-                max_depth -= 1
-            }
+    result := make([]string, 0)
+    for _, x := range stringList {
+        if strings.Index(substring, x) != -1 {
+            result = append(result, x)
         }
-        return max_depth
-    }
-    result := make([]int, 0)
-    for _, x := range strings.Split(paren_string, " ") {
-        result = append(result, parse_paren_group(x))
     }
     return result
-
 }
 
-func ExampleTestParseNestedParens(t *testing.T) {
+func ExampleTestFilterBySubstring(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([]int{2, 3, 1, 3}, ParseNestedParens("(()()) ((())) () ((())()())"))
+    assert.Equal([]string{}, FilterBySubstring([]string{}, "a"))
+    assert.Equal([]string{"abc", "bacd", "array"}, FilterBySubstring([]string{"abc", "bacd", "cde", "array"}, "a"))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestParseNestedParens(t)
+    ExampleTestFilterBySubstring(t)
 }
