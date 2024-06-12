a07d29687b0e9b60ca633700ef382d620a84163a
swe-bench <>
2024-06-12 16:19:40 -0400

    Task instance Go/92

--- mymain.go
+++ mymain.go
@@ -4,40 +4,50 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
-import (
-    "regexp"
-)
 
-// You'll be given a string of words, and your task is to count the number
-// of boredoms. A boredom is a sentence that starts with the word "I".
-// Sentences are delimited by '.', '?' or '!'.
+// Create a function that takes 3 numbers.
+// Returns true if one of the numbers is equal to the sum of the other two, and all numbers are integers.
+// Returns false in any other cases.
 // 
-// For example:
-// >>> IsBored("Hello world")
-// 0
-// >>> IsBored("The sky is blue. The sun is shining. I love this weather")
-// 1
-func IsBored(S string) int {
+// Examples
+// AnyInt(5, 2, 7) ➞ true
+// 
+// AnyInt(3, 2, 2) ➞ false
+// 
+// AnyInt(3, -2, 1) ➞ true
+// 
+// AnyInt(3.6, -2.2, 2) ➞ false
+func AnyInt(x, y, z interface{}) bool {
 
-    r, _ := regexp.Compile(`[.?!]\s*`)
-    sentences := r.Split(S, -1)
-    sum := 0
-    for _, s := range sentences {
-        if len(s) >= 2 && s[:2] == " I" {
-            sum++
-        }
+    xx, ok := x.(int)
+    if !ok {
+        return false
     }
-    return sum
+    yy, ok := y.(int)
+    if !ok {
+        return false
+    }
+    zz, ok := z.(int)
+    if !ok {
+        return false
+    }
+
+    if (xx+yy == zz) || (yy+zz == xx) {
+        return true
+    }
+    return false
 }
 
-func ExampleTestIsBored(t *testing.T) {
+func ExampleTestAnyInt(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(0, IsBored("Hello world"), "Test 1")
-    assert.Equal(1, IsBored("The sky is blue. The sun is shining. I love this weather"), "Test 3")
+    assert.Equal(true, AnyInt(5, 2, 7))
+    assert.Equal(false, AnyInt(3, 2, 2))
+    assert.Equal(true, AnyInt(3, -2, 1))
+    assert.Equal(false, AnyInt(3.6, -2.2, 2))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestIsBored(t)
+    ExampleTestAnyInt(t)
 }
