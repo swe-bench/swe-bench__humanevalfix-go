f0d178477a5662c774915a1ede0a0250ebd51ea1
swe-bench <>
2024-06-12 16:19:33 -0400

    Task instance Go/60

--- mymain.go
+++ mymain.go
@@ -5,41 +5,37 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
-// Return the largest prime factor of n. Assume n > 1 and is not a prime.
-// >>> LargestPrimeFactor(13195)
-// 29
-// >>> LargestPrimeFactor(2048)
-// 2
-func LargestPrimeFactor(n int) int {
+// SumToN is a function that sums numbers from 1 to n.
+// >>> SumToN(30)
+// 465
+// >>> SumToN(100)
+// 5050
+// >>> SumToN(5)
+// 15
+// >>> SumToN(10)
+// 55
+// >>> SumToN(1)
+// 1
+func SumToN(n int) int {
 
-    isPrime := func(n int) bool {
-        for i := 2; i < int(math.Pow(float64(n), 0.5)+1); i++ {
-            if n%i == 0 {
-                return false
-            }
-        }
-        return true
-    }
-
-    largest := 1
-    for j := 2; j < n + 1; j++ {
-		if n % j == 0 && isPrime(n) {
-			if j > largest {
-				largest = j
-			}
-		}
+    if n <= 0 {
+		return 0
+	} else {
+		return 1 + SumToN(n - 1)
 	}
-    return largest
 }
 
-func ExampleTestLargestPrimeFactor(t *testing.T) {
+func ExampleTestSumToN(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(2, LargestPrimeFactor(2048))
-    assert.Equal(29, LargestPrimeFactor(13195))
+    assert.Equal(1, SumToN(1))
+    assert.Equal(15, SumToN(5))
+    assert.Equal(55, SumToN(10))
+    assert.Equal(465, SumToN(30))
+    assert.Equal(5050, SumToN(100))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestLargestPrimeFactor(t)
+    ExampleTestSumToN(t)
 }
