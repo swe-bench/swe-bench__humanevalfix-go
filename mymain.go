c60843c98773de85a6579df96285c6491a08215e
swe-bench <>
2024-06-12 16:19:30 -0400

    Task instance Go/48

--- mymain.go
+++ mymain.go
@@ -4,33 +4,36 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
-import (
-	"sort"
-)
 
-// Return Median of elements in the list l.
-// >>> Median([3, 1, 2, 4, 5])
-// 3.0
-// >>> Median([-10, 4, 6, 1000, 10, 20])
-// 15.0
-func Median(l []int) float64 {
+// Checks if given string is a palindrome
+// >>> IsPalindrome('')
+// true
+// >>> IsPalindrome('aba')
+// true
+// >>> IsPalindrome('aaaaa')
+// true
+// >>> IsPalindrome('zbcd')
+// false
+func IsPalindrome(text string) bool {
 
-    sort.Ints(l)
-	if len(l)%2==1{
-		return float64(l[len(l)/2])
-	}else{
-		return float64(l[len(l)-1/2]+l[len(l)/2])/2.0
-	}
+    runes := []rune(text)
+    result := make([]rune, 0)
+    for i := len(runes) - 1; i > 0; i-- {
+        result = append(result, runes[i])
+    }
+    return text == string(result)
 }
 
-func ExampleTestMedian(t *testing.T) {
-	assert := assert.New(t)
-	assert.Equal(3.0, Median([]int{3, 1, 2, 4, 5}))
-	assert.Equal(8.0, Median([]int{-10, 4, 6, 1000, 10, 20}))
+func ExampleTestIsPalindrome(t *testing.T) {
+    assert := assert.New(t)
+    assert.Equal(true, IsPalindrome(""))
+    assert.Equal(true, IsPalindrome("aba"))
+    assert.Equal(true, IsPalindrome("aaaaa"))
+    assert.Equal(false, IsPalindrome("zbcd"))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestMedian(t)
+    ExampleTestIsPalindrome(t)
 }
