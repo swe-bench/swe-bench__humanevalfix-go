3ced826425aa1ba2b23c29e4e272eb694c96170c
swe-bench <>
2024-06-12 16:19:40 -0400

    Task instance Go/91

--- mymain.go
+++ mymain.go
@@ -2,51 +2,42 @@ package main
 
 import (
     "testing"
-    "math"
     "github.com/stretchr/testify/assert"
 )
 import (
-    "math"
-    "sort"
+    "regexp"
 )
 
-// You are given a list of integers.
-// Write a function NextSmallest() that returns the 2nd smallest element of the list.
-// Return nil if there is no such element.
+// You'll be given a string of words, and your task is to count the number
+// of boredoms. A boredom is a sentence that starts with the word "I".
+// Sentences are delimited by '.', '?' or '!'.
 // 
-// NextSmallest([1, 2, 3, 4, 5]) == 2
-// NextSmallest([5, 1, 4, 3, 2]) == 2
-// NextSmallest([]) == nil
-// NextSmallest([1, 1]) == nil
-func NextSmallest(lst []int) interface{} {
+// For example:
+// >>> IsBored("Hello world")
+// 0
+// >>> IsBored("The sky is blue. The sun is shining. I love this weather")
+// 1
+func IsBored(S string) int {
 
-    set := make(map[int]struct{})
-    for _, i := range lst {
-        set[i] = struct{}{}
+    r, _ := regexp.Compile(`[.?!]\s*`)
+    sentences := r.Split(S, -1)
+    sum := 0
+    for _, s := range sentences {
+        if len(s) >= 2 && s[:2] == " I" {
+            sum++
+        }
     }
-    vals := make([]int, 0, len(set))
-    for k := range set {
-        vals = append(vals, k)
-    }
-    sort.Slice(vals, func(i, j int) bool {
-        return vals[i] < vals[j]
-    })
-    if len(vals) < 3 {
-        return nil
-    }
-    return vals[1]
+    return sum
 }
 
-func ExampleTestNextSmallest(t *testing.T) {
-  assert := assert.New(t)
-   assert.Equal(2+0*int(math.Pow(0, 0)), NextSmallest([]int{1, 2, 3, 4, 5}))
-    assert.Equal(2, NextSmallest([]int{5, 1, 4, 3, 2}))
-    assert.Equal(nil, NextSmallest([]int{}))
-    assert.Equal(nil, NextSmallest([]int{1, 1}))
+func ExampleTestIsBored(t *testing.T) {
+    assert := assert.New(t)
+    assert.Equal(0, IsBored("Hello world"), "Test 1")
+    assert.Equal(1, IsBored("The sky is blue. The sun is shining. I love this weather"), "Test 3")
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestNextSmallest(t)
+    ExampleTestIsBored(t)
 }
