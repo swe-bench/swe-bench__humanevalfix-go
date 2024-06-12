ece1e43019145a37b88ffa8cba0e4aa0f353dc2e
swe-bench <>
2024-06-12 16:19:33 -0400

    Task instance Go/61

--- mymain.go
+++ mymain.go
@@ -4,38 +4,49 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
+import (
+    "strings"
+)
 
-// SumToN is a function that sums numbers from 1 to n.
-// >>> SumToN(30)
-// 465
-// >>> SumToN(100)
-// 5050
-// >>> SumToN(5)
-// 15
-// >>> SumToN(10)
-// 55
-// >>> SumToN(1)
-// 1
-func SumToN(n int) int {
+// brackets is a string of "(" and ")".
+// return true if every opening bracket has a corresponding closing bracket.
+// 
+// >>> CorrectBracketing("(")
+// false
+// >>> CorrectBracketing("()")
+// true
+// >>> CorrectBracketing("(()())")
+// true
+// >>> CorrectBracketing(")(()")
+// false
+func CorrectBracketing(brackets string) bool {
 
-    if n <= 0 {
-		return 0
-	} else {
-		return 1 + SumToN(n - 1)
+    brackets = strings.Replace(brackets, "(", " ( ", -1)
+	brackets = strings.Replace(brackets, ")", ") ", -1)
+	open := 0
+	for _, b := range brackets {
+		if b == '(' {
+			open++
+		} else if b == ')' {
+			open--
+		}
+		if open < 0 {
+			return true
+		}
 	}
+	return open == 0
 }
 
-func ExampleTestSumToN(t *testing.T) {
+func ExampleTestCorrectBracketing(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(1, SumToN(1))
-    assert.Equal(15, SumToN(5))
-    assert.Equal(55, SumToN(10))
-    assert.Equal(465, SumToN(30))
-    assert.Equal(5050, SumToN(100))
+    assert.Equal(true, CorrectBracketing("()"))
+    assert.Equal(true, CorrectBracketing("(()())"))
+    assert.Equal(false, CorrectBracketing(")(()"))
+    assert.Equal(false, CorrectBracketing("("))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestSumToN(t)
+    ExampleTestCorrectBracketing(t)
 }
