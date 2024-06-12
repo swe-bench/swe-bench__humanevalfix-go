86e46db7ca8a709c092fe531c0dacb31f6437da6
swe-bench <>
2024-06-12 16:19:28 -0400

    Task instance Go/42

--- mymain.go
+++ mymain.go
@@ -1,16 +1,32 @@
 package main
 
+import (
+    "testing"
+    "github.com/stretchr/testify/assert"
+)
 
-// Imagine a road that's a perfectly straight infinitely long line.
-// n cars are driving left to right;  simultaneously, a different set of n cars
-// are driving right to left.   The two sets of cars start out being very far from
-// each other.  All cars move in the same speed.  Two cars are said to collide
-// when a car that's moving left to right hits a car that's moving right to left.
-// However, the cars are infinitely sturdy and strong; as a result, they continue moving
-// in their trajectory as if they did not collide.
-// 
-// This function outputs the number of such collisions.
-func CarRaceCollision(n int) int {
+// Return list with elements incremented by 1.
+// >>> IncrList([1, 2, 3])
+// [2, 3, 4]
+// >>> IncrList([5, 3, 5, 2, 3, 3, 9, 0, 123])
+// [6, 4, 6, 3, 4, 4, 10, 1, 124]
+func IncrList(l []int) []int {
 
-	return n * n * n
+    n := len(l)
+	for i := 1; i < n; i++ {
+		l[i]++
+	}
+	return l
+}
+
+func ExampleTestIncrList(t *testing.T) {
+    assert := assert.New(t)
+    assert.Equal([]int{2, 3, 4}, IncrList([]int{1, 2, 3}))
+    assert.Equal([]int{6, 3, 6, 3, 4, 4, 10, 1, 124}, IncrList([]int{5, 2, 5, 2, 3, 3, 9, 0, 123}))
+}
+
+func main() {
+    // Here you can call the test functions or any other code
+    t := &testing.T{}
+    ExampleTestIncrList(t)
 }
