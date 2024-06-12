9d3cb638ea1ef30de49bfc0343569ed85752cac1
swe-bench <>
2024-06-12 16:19:33 -0400

    Task instance Go/63

--- mymain.go
+++ mymain.go
@@ -5,31 +5,44 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
-// xs represent coefficients of a polynomial.
-// xs[0] + xs[1] * x + xs[2] * x^2 + ....
-// Return Derivative of this polynomial in the same form.
-// >>> Derivative([3, 1, 2, 4, 5])
-// [1, 4, 12, 20]
-// >>> Derivative([1, 2, 3])
-// [2, 6]
-func Derivative(xs []int) []int {
+// The FibFib number sequence is a sequence similar to the Fibbonacci sequnece that's defined as follows:
+// Fibfib(0) == 0
+// Fibfib(1) == 0
+// Fibfib(2) == 1
+// Fibfib(n) == Fibfib(n-1) + Fibfib(n-2) + Fibfib(n-3).
+// Please write a function to efficiently compute the n-th element of the Fibfib number sequence.
+// >>> Fibfib(1)
+// 0
+// >>> Fibfib(5)
+// 4
+// >>> Fibfib(8)
+// 24
+func Fibfib(n int) int {
 
-    l := len(xs)
-	y := make([]int, l - 1)
-	for i := 0; i < l - 1; i++ {
-		y[i] = xs[i + 1] * i
+    if n <= 0 {
+		return 0
 	}
-	return y
+    switch n {
+	case 0:
+		return 0
+	case 1:
+		return 1
+	case 2:
+		return 2
+	default:
+		return Fibfib(n-1) + Fibfib(n-2) + Fibfib(n-3)
+	}
 }
 
-func ExampleTestDerivative(t *testing.T) {
+func ExampleTestFibfib(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([]int{1, 4, 12, 20}, Derivative([]int{3, 1, 2, 4, 5}))
-    assert.Equal([]int{2, 6}, Derivative([]int{1, 2, 3}))
+    assert.Equal(0, Fibfib(1))
+    assert.Equal(4, Fibfib(5))
+    assert.Equal(24, Fibfib(8))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestDerivative(t)
+    ExampleTestFibfib(t)
 }
