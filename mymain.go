b43bbcc8c112573f864e22137fe245b640e2793a
swe-bench <>
2024-06-12 16:19:37 -0400

    Task instance Go/79

--- mymain.go
+++ mymain.go
@@ -4,46 +4,33 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
+import (
+	"fmt"
+)
 
-// You have been tasked to write a function that receives
-// a hexadecimal number as a string and counts the number of hexadecimal
-// digits that are primes (prime number, or a prime, is a natural number
-// greater than 1 that is not a product of two smaller natural numbers).
-// Hexadecimal digits are 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, A, B, C, D, E, F.
-// Prime numbers are 2, 3, 5, 7, 11, 13, 17,...
-// So you have to determine a number of the following digits: 2, 3, 5, 7,
-// B (=decimal 11), D (=decimal 13).
-// Note: you may assume the input is always correct or empty string,
-// and symbols A,B,C,D,E,F are always uppercase.
+// You will be given a number in decimal form and your task is to convert it to
+// binary format. The function should return a string, with each character representing a binary
+// number. Each character in the string will be '0' or '1'.
+// 
+// There will be an extra couple of characters 'db' at the beginning and at the end of the string.
+// The extra characters are there to help with the format.
+// 
 // Examples:
-// For num = "AB" the output should be 1.
-// For num = "1077E" the output should be 2.
-// For num = "ABED1A33" the output should be 4.
-// For num = "123456789ABCDEF0" the output should be 6.
-// For num = "2020" the output should be 2.
-func HexKey(num string) int {
+// DecimalToBinary(15)   # returns "db1111db"
+// DecimalToBinary(32)   # returns "db100000db"
+func DecimalToBinary(decimal int) string {
 
-    primes := map[int32]interface{}{'2': nil, '3': nil, '5': nil, '7': nil, 'B': nil, 'D': nil}
-	total := 1
-	for _, c := range num {
-		if _, ok := primes[c]; ok {
-			total++
-		}
-	}
-	return total
+    return fmt.Sprintf("db%bd", decimal)
 }
 
-func ExampleTestHexKey(t *testing.T) {
+func ExampleTestDecimalToBinary(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(1, HexKey("AB"))
-    assert.Equal(2, HexKey("1077E"))
-    assert.Equal(4, HexKey("ABED1A33"))
-    assert.Equal(2, HexKey("2020"))
-    assert.Equal(6, HexKey("123456789ABCDEF0"))
+    assert.Equal("db100000db", DecimalToBinary(32))
+    assert.Equal("db1111db", DecimalToBinary(15))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestHexKey(t)
+    ExampleTestDecimalToBinary(t)
 }
