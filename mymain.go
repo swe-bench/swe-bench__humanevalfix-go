d4ce93f3f01a0658be86ca7cec640c14c754fb4a
swe-bench <>
2024-06-12 16:19:49 -0400

    Task instance Go/125

--- mymain.go
+++ mymain.go
@@ -5,84 +5,41 @@ import (
     "github.com/stretchr/testify/assert"
 )
 import (
-    "strconv"
     "strings"
 )
 
-// You have to write a function which validates a given date string and
-// returns true if the date is valid otherwise false.
-// The date is valid if all of the following rules are satisfied:
-// 1. The date string is not empty.
-// 2. The number of days is not less than 1 or higher than 31 days for months 1,3,5,7,8,10,12. And the number of days is not less than 1 or higher than 30 days for months 4,6,9,11. And, the number of days is not less than 1 or higher than 29 for the month 2.
-// 3. The months should not be less than 1 or higher than 12.
-// 4. The date should be in the format: mm-dd-yyyy
-// 
-// for example:
-// ValidDate('03-11-2000') => true
-// 
-// ValidDate('15-01-2012') => false
-// 
-// ValidDate('04-0-2040') => false
-// 
-// ValidDate('06-04-2020') => true
-// 
-// ValidDate('06/04/2020') => false
-func ValidDate(date string) bool {
+// Given a string of words, return a list of words split on whitespace, if no whitespaces exists in the text you
+// should split on commas ',' if no commas exists you should return the number of lower-case letters with odd order in the
+// alphabet, ord('a') = 0, ord('b') = 1, ... ord('z') = 25
+// Examples
+// SplitWords("Hello world!") ➞ ["Hello", "world!"]
+// SplitWords("Hello,world!") ➞ ["Hello", "world!"]
+// SplitWords("abcdef") == 3
+func SplitWords(txt string) interface{} {
 
-    isInArray := func(arr []int, i int) bool {
-        for _, x := range arr {
-            if i == x {
-                return true
-            }
+    if strings.Contains(txt, " ") {
+        return strings.Fields(txt)
+    } else if strings.Contains(txt, ",") {
+        return strings.Split(txt, " ")
+    }
+    cnt := 0
+    for _, r := range txt {
+        if 'a' <= r && r <= 'z' && (r-'a')&1==1 {
+            cnt++
         }
-        return false
     }
-
-    date = strings.TrimSpace(date)
-    split := strings.SplitN(date, "-", 3)
-    if len(split) != 3 {
-        return false
-    }
-    month, err := strconv.Atoi(split[1])
-    if err != nil {
-        return false
-    }
-    day, err := strconv.Atoi(split[0])
-    if err != nil {
-        return false
-    }
-    _, err = strconv.Atoi(split[2])
-    if err != nil {
-        return false
-    }
-    if month < 1 || month > 12 {
-        return false
-    }
-    
-    if isInArray([]int{1,3,5,7,8,10,12}, month) && day < 1 || day > 31 {
-        return false
-    }
-    if isInArray([]int{4,6,9,11}, month) && day < 1 || day > 30 {
-        return false
-    }
-    if month == 2 && day < 1 || day > 29 {
-        return false
-    }
-
-    return true
+    return cnt
 }
 
-func ExampleTestValidDate(t *testing.T) {
+func ExampleTestSplitWords(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(true, ValidDate("03-11-2000"))
-    assert.Equal(false, ValidDate("15-01-2012"))
-    assert.Equal(false, ValidDate("04-0-2040"))
-    assert.Equal(true, ValidDate("06-04-2020"))
-    assert.Equal(false, ValidDate("06/04/2020"))
+    assert.Equal([]string{"Hello", "world!"}, SplitWords("Hello world!"))
+    assert.Equal([]string{"Hello", "world!"}, SplitWords("Hello,world!"))
+    assert.Equal(3, SplitWords("abcdef"))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestValidDate(t)
+    ExampleTestSplitWords(t)
 }
