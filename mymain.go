bb4e7de4960ebea905c5088346152e3604c00c34
swe-bench <>
2024-06-12 16:19:47 -0400

    Task instance Go/116

--- mymain.go
+++ mymain.go
@@ -5,62 +5,48 @@ import (
     "github.com/stretchr/testify/assert"
 )
 import (
-    "math"
+    "fmt"
+    "sort"
 )
 
-// You are given a rectangular grid of wells. Each row represents a single well,
-// and each 1 in a row represents a single unit of water.
-// Each well has a corresponding bucket that can be used to extract water from it,
-// and all buckets have the same capacity.
-// Your task is to use the buckets to empty the wells.
-// Output the number of times you need to lower the buckets.
+// In this Kata, you have to sort an array of non-negative integers according to
+// number of ones in their binary representation in ascending order.
+// For similar number of ones, sort based on decimal value.
 // 
-// Example 1:
-// Input:
-// grid : [[0,0,1,0], [0,1,0,0], [1,1,1,1]]
-// bucket_capacity : 1
-// Output: 6
-// 
-// Example 2:
-// Input:
-// grid : [[0,0,1,1], [0,0,0,0], [1,1,1,1], [0,1,1,1]]
-// bucket_capacity : 2
-// Output: 5
-// 
-// Example 3:
-// Input:
-// grid : [[0,0,0], [0,0,0]]
-// bucket_capacity : 5
-// Output: 0
-// 
-// Constraints:
-// * all wells have the same length
-// * 1 <= grid.length <= 10^2
-// * 1 <= grid[:,1].length <= 10^2
-// * grid[i][j] -> 0 | 1
-// * 1 <= capacity <= 10
-func MaxFill(grid [][]int, capacity int) int {
+// It must be implemented like this:
+// >>> SortArray([1, 5, 2, 3, 4]) == [1, 2, 3, 4, 5]
+// >>> SortArray([-2, -3, -4, -5, -6]) == [-6, -5, -4, -3, -2]
+// >>> SortArray([1, 0, 2, 3, 4]) [0, 1, 2, 3, 4]
+func SortArray(arr []int) []int {
 
-    result := 0
-    for _, arr := range grid {
-        sum := 0
-        for _, i := range arr {
-            sum += i
+    sort.Slice(arr, func(i, j int) bool {
+        return arr[i] < arr[j]
+    })
+    sort.Slice(arr, func(i, j int) bool {
+        key := func(x int) int {
+            b := fmt.Sprintf("%b", x)
+            cnt := 0
+            for _, r := range b {
+                if r == '1' {
+                    cnt++
+                }
+            }
+            return cnt
         }
-        result += int(math.Floor(float64(sum) / float64(capacity)))
-    }
-    return result
+        return key(arr[j]) < key(arr[i])
+    })
+    return arr
 }
 
-func ExampleTestMaxFill(t *testing.T) {
-  assert := assert.New(t)
-  assert.Equal(6, MaxFill([][]int{{0,0,1,0}, {0,1,0,0}, {1,1,1,1}}, 1))
-  assert.Equal(5, MaxFill([][]int{{0,0,1,1}, {0,0,0,0}, {1,1,1,1}, {0,1,1,1}}, 2))
-  assert.Equal(0, MaxFill([][]int{{0,0,0}, {0,0,0}}, 5))
+func ExampleTestSortArray(t *testing.T) {
+    assert := assert.New(t)
+    assert.Equal([]int{1, 2, 4, 3, 5}, SortArray([]int{1,5,2,3,4}))
+    assert.Equal([]int{-4, -2, -6, -5, -3}, SortArray([]int{-2,-3,-4,-5,-6}))
+    assert.Equal([]int{0, 1, 2, 4, 3}, SortArray([]int{1,0,2,3,4}))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestMaxFill(t)
+    ExampleTestSortArray(t)
 }
