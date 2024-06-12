7716e29d10c609698491d9f16aa0afc83ebae25a
swe-bench <>
2024-06-12 16:19:56 -0400

    Task instance Go/157

--- mymain.go
+++ mymain.go
@@ -4,46 +4,27 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
-import (
-    "strings"
-)
 
-// Given a positive integer, obtain its roman numeral equivalent as a string,
-// and return it in lowercase.
-// Restrictions: 1 <= num <= 1000
-// 
-// Examples:
-// >>> IntToMiniRoman(19) == 'xix'
-// >>> IntToMiniRoman(152) == 'clii'
-// >>> IntToMiniRoman(426) == 'cdxxvi'
-func IntToMiniRoman(number int) string {
+// Given the lengths of the three sides of a triangle. Return true if the three
+// sides form a right-angled triangle, false otherwise.
+// A right-angled triangle is a triangle in which one angle is right angle or
+// 90 degree.
+// Example:
+// RightAngleTriangle(3, 4, 5) == true
+// RightAngleTriangle(1, 2, 3) == false
+func RightAngleTriangle(a, b, c int) bool {
 
-    num := []int{1, 4, 5, 9, 10, 40, 50, 90,  
-           100, 400, 500, 900, 1000}
-    sym := []string{"I", "IV", "V", "IX", "X", "XL",  
-           "L", "XC", "C", "CD", "D", "CM", "M"}
-    i := 12
-    res := ""
-    for number != 0 {
-        div := number / num[i] 
-        for div != 0 {
-            res += sym[i] 
-            div--
-        }
-        i--
-    }
-    return strings.ToLower(res)
+    return c*c == a*a + b*b
 }
 
-func ExampleTestIntToMiniRoman(t *testing.T) {
+func ExampleTestRightAngleTriangle(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal("xix", IntToMiniRoman(19))
-    assert.Equal("clii", IntToMiniRoman(152))
-    assert.Equal("cdxxvi", IntToMiniRoman(426))
+    assert.Equal(true, RightAngleTriangle(3, 4, 5))
+    assert.Equal(false, RightAngleTriangle(1, 2, 3))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestIntToMiniRoman(t)
+    ExampleTestRightAngleTriangle(t)
 }
