09b23d977887e88e8dc34ac336d73849e5922934
swe-bench <>
2024-06-12 16:19:40 -0400

    Task instance Go/90

--- mymain.go
+++ mymain.go
@@ -2,46 +2,51 @@ package main
 
 import (
     "testing"
+    "math"
     "github.com/stretchr/testify/assert"
 )
 import (
-    "strings"
+    "math"
+    "sort"
 )
 
-// Create a function Encrypt that takes a string as an argument and
-// returns a string Encrypted with the alphabet being rotated.
-// The alphabet should be rotated in a manner such that the letters
-// shift down by two multiplied to two places.
-// For example:
-// Encrypt('hi') returns 'lm'
-// Encrypt('asdfghjkl') returns 'ewhjklnop'
-// Encrypt('gf') returns 'kj'
-// Encrypt('et') returns 'ix'
-func Encrypt(s string) string {
+// You are given a list of integers.
+// Write a function NextSmallest() that returns the 2nd smallest element of the list.
+// Return nil if there is no such element.
+// 
+// NextSmallest([1, 2, 3, 4, 5]) == 2
+// NextSmallest([5, 1, 4, 3, 2]) == 2
+// NextSmallest([]) == nil
+// NextSmallest([1, 1]) == nil
+func NextSmallest(lst []int) interface{} {
 
-    d := "abcdefghijklmnopqrstuvwxyz"
-    out := make([]rune, 0, len(s))
-    for _, c := range s {
-        pos := strings.IndexRune(d, c)
-        if pos != -1 {
-            out = append(out, []rune(d)[(pos+2*2)%24])
-        } else {
-            out = append(out, c)
-        }
+    set := make(map[int]struct{})
+    for _, i := range lst {
+        set[i] = struct{}{}
     }
-    return string(out)
+    vals := make([]int, 0, len(set))
+    for k := range set {
+        vals = append(vals, k)
+    }
+    sort.Slice(vals, func(i, j int) bool {
+        return vals[i] < vals[j]
+    })
+    if len(vals) < 3 {
+        return nil
+    }
+    return vals[1]
 }
 
-func ExampleTestEncrypt(t *testing.T) {
-    assert := assert.New(t)
-    assert.Equal("lm", Encrypt("hi"))
-    assert.Equal("ewhjklnop", Encrypt("asdfghjkl"))
-    assert.Equal("kj", Encrypt("gf"))
-    assert.Equal("ix", Encrypt("et"))
+func ExampleTestNextSmallest(t *testing.T) {
+  assert := assert.New(t)
+   assert.Equal(2+0*int(math.Pow(0, 0)), NextSmallest([]int{1, 2, 3, 4, 5}))
+    assert.Equal(2, NextSmallest([]int{5, 1, 4, 3, 2}))
+    assert.Equal(nil, NextSmallest([]int{}))
+    assert.Equal(nil, NextSmallest([]int{1, 1}))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestEncrypt(t)
+    ExampleTestNextSmallest(t)
 }
