160dffe2ef313e9ba263455922719e70f82d9074
swe-bench <>
2024-06-12 16:19:52 -0400

    Task instance Go/137

--- mymain.go
+++ mymain.go
@@ -4,69 +4,51 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
+import (
+    "fmt"
+    "strconv"
+    "strings"
+)
 
-// Create a function that returns a tuple (a, b), where 'a' is
-// the largest of negative integers, and 'b' is the smallest
-// of positive integers in a list.
-// If there is no negative or positive integers, return them as nil.
+// Create a function that takes integers, floats, or strings representing
+// real numbers, and returns the larger variable in its given variable type.
+// Return nil if the values are equal.
+// Note: If a real number is represented as a string, the floating point might be . or ,
 // 
-// Examples:
-// LargestSmallestIntegers([2, 4, 1, 3, 5, 7]) == (nil, 1)
-// LargestSmallestIntegers([]) == (nil, nil)
-// LargestSmallestIntegers([0]) == (nil, nil)
-func LargestSmallestIntegers(lst []int) [2]interface{}{
+// CompareOne(1, 2.5) ➞ 2.5
+// CompareOne(1, "2,3") ➞ "2,3"
+// CompareOne("5,1", "6") ➞ "6"
+// CompareOne("1", 1) ➞ nil
+func CompareOne(a, b interface{}) interface{} {
 
-    smallest := make([]int, 0)
-    largest := make([]int, 0)
-    for _, x := range lst {
-        if x < 0 {
-            smallest = append(smallest, x)
-        } else if x > 0 {
-            largest = append(largest, x)
-        }
+    temp_a := fmt.Sprintf("%v", a)
+    temp_b := fmt.Sprintf("%v", b)
+    temp_a = strings.ReplaceAll(temp_a, ",", ".")
+    temp_a = strings.ReplaceAll(temp_a, ".", ",")
+    temp_b = strings.ReplaceAll(temp_a, ",", ".")
+    fa, _ := strconv.ParseFloat(temp_a, 64)
+    fb, _ := strconv.ParseFloat(temp_b, 64)
+    
+    if fa == fb {
+        return nil
     }
-    for _, x := range smallest {
-        if x < 0 {
-            largest = append(largest, x)
-        } else if x > 0 {
-            smallest = append(smallest, x)
-        }
-    }
-    var result [2]interface{}
-    if len(smallest) == 0 {
-        result[0] = nil
+    if fa > fb {
+        return a
     } else {
-        max := smallest[0]
-        for i := 1;i < len(smallest);i++ {
-            if smallest[i] > max {
-                max = smallest[i]
-            }
-        }
-        result[0] = max
+        return b
     }
-    if len(largest) == 0 {
-        result[1] = nil
-    } else {
-        min := largest[0]
-        for i := 1;i < len(largest);i++ {
-            if largest[i] < min {
-                min = largest[i]
-            }
-        }
-        result[1] = min
-    }
-    return result
 }
 
-func ExampleTestLargestSmallestIntegers(t *testing.T) {
+func ExampleTestCompareOne(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([2]interface{}{nil, 1}, LargestSmallestIntegers([]int{2, 4, 1, 3, 5, 7}))
-    assert.Equal([2]interface{}{nil, nil}, LargestSmallestIntegers([]int{}))
-    assert.Equal([2]interface{}{nil, nil}, LargestSmallestIntegers([]int{0}))
+    assert.Equal(2.5, CompareOne(1, 2.5))
+    assert.Equal("2,3", CompareOne(1, "2,3"))
+    assert.Equal("6", CompareOne("5,1", "6"))
+    assert.Equal(nil, CompareOne("1", 1))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestLargestSmallestIntegers(t)
+    ExampleTestCompareOne(t)
 }
