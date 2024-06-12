2e87ce7df96c736ca3fdf6a86439d720cbdf6f14
swe-bench <>
2024-06-12 16:19:39 -0400

    Task instance Go/87

--- mymain.go
+++ mymain.go
@@ -6,41 +6,58 @@ import (
 )
 import (
     "sort"
-    "strings"
 )
 
-// Write a function that takes a string and returns an ordered version of it.
-// Ordered version of string, is a string where all words (separated by space)
-// are replaced by a new word where all the characters arranged in
-// ascending order based on ascii value.
-// Note: You should keep the order of words and blank spaces in the sentence.
+// You are given a 2 dimensional data, as a nested lists,
+// which is similar to matrix, however, unlike matrices,
+// each row may contain a different number of columns.
+// Given lst, and integer x, find integers x in the list,
+// and return list of tuples, [(x1, y1), (x2, y2) ...] such that
+// each tuple is a coordinate - (row, columns), starting with 0.
+// Sort coordinates initially by rows in ascending order.
+// Also, sort coordinates of the row by columns in descending order.
 // 
-// For example:
-// AntiShuffle('Hi') returns 'Hi'
-// AntiShuffle('hello') returns 'ehllo'
-// AntiShuffle('Hello World!!!') returns 'Hello !!!Wdlor'
-func AntiShuffle(s string) string {
+// Examples:
+// GetRow([
+// [1,2,3,4,5,6],
+// [1,2,3,4,1,6],
+// [1,2,3,4,5,1]
+// ], 1) == [(0, 0), (1, 4), (1, 0), (2, 5), (2, 0)]
+// GetRow([], 1) == []
+// GetRow([[], [1], [1, 2, 3]], 3) == [(2, 2)]
+func GetRow(lst [][]int, x int) [][2]int {
 
-    strs := make([]string, 0)
-    for _, i := range strings.Fields(s) {
-        word := []rune(i)
-        sort.Slice(word, func(i, j int) bool {
-            return word[i] < word[j]
-        })
-        strs = append(strs, string(word))
+    coords := make([][2]int, 0)
+    for i, row := range lst {
+        for j, item := range row {
+            if item == x {
+                coords = append(coords, [2]int{i, j})
+            }
+        }
     }
-    return strings.Join(strs, "")
+    sort.Slice(coords, func(i, j int) bool {
+        if coords[i][0] == coords[j][0] {
+            return coords[j][1] > coords[i][1]
+        }
+        return coords[j][0] < coords[i][0]
+    })
+
+    return coords
 }
 
-func ExampleTestAntiShuffle(t *testing.T) {
+func ExampleTestGetRow(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal("Hi", AntiShuffle("Hi"))
-    assert.Equal("ehllo", AntiShuffle("hello"))
-    assert.Equal("Hello !!!Wdlor", AntiShuffle("Hello World!!!"))
+    assert.Equal([][2]int{{0, 0}, {1, 4}, {1, 0}, {2, 5}, {2, 0}}, GetRow([][]int{
+        {1, 2, 3, 4, 5, 6},
+        {1, 2, 3, 4, 1, 6},
+        {1, 2, 3, 4, 5, 1},
+    }, 1))
+    assert.Equal([][2]int{}, GetRow([][]int{{}}, 1))
+    assert.Equal([][2]int{{2, 2}}, GetRow([][]int{{}, {1}, {1, 2, 3}}, 3))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestAntiShuffle(t)
+    ExampleTestGetRow(t)
 }
