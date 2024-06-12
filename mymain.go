b74892c29bdc5e2be5446ad2c29e7b6e70391525
swe-bench <>
2024-06-12 16:19:21 -0400

    Task instance Go/10

--- mymain.go
+++ mymain.go
@@ -5,37 +5,54 @@ import (
     "github.com/stretchr/testify/assert"
 )
 import (
-    "math"
+    "strings"
 )
 
-// From a given list of integers, generate a list of rolling maximum element found until given moment
-// in the sequence.
-// >>> RollingMax([1, 2, 3, 2, 3, 4, 2])
-// [1, 2, 3, 3, 3, 4, 4]
-func RollingMax(numbers []int) []int {
-
-    running_max := math.MinInt32
-    result := make([]int, 0)
-
-    for _, n := range numbers {
-        if running_max == math.MinInt32 {
-            running_max = n
-        } else {
-            running_max = int(math.Max(numbers))
-        }
-        result = append(result, running_max)
+// Test if given string is a palindrome.
+func IsPalindrome(str string) bool {
+    runes := []rune(str)
+    result := make([]rune, 0)
+    for i := len(runes) - 1; i >= 0; i-- {
+        result = append(result, runes[i])
     }
+    return str == string(result)
+}
+// Find the shortest palindrome that begins with a supplied string.
+// Algorithm idea is simple:
+// - Find the longest postfix of supplied string that is a palindrome.
+// - Append to the end of the string reverse of a string prefix that comes before the palindromic suffix.
+// >>> MakePalindrome('')
+// ''
+// >>> MakePalindrome('cat')
+// 'catac'
+// >>> MakePalindrome('cata')
+// 'catac'
+func MakePalindrome(str string) string {
 
-    return result
+    if strings.TrimSpace(str) == "" {
+        return ""
+    }
+    beginning_of_suffix := 0
+    runes := []rune(str)
+    for !IsPalindrome(strings) {
+        beginning_of_suffix += 1
+    }
+    result := make([]rune, 0)
+    for i := len(str[:beginning_of_suffix]) - 1; i >= 0; i-- {
+        result = append(result, runes[i])
+    }
+    return str + string(result)
 }
 
-func ExampleTestRollingMax(t *testing.T) {
+func ExampleTestMakePalindrome(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([]int{1, 2, 3,3, 3, 4, 4}, RollingMax([]int{1, 2, 3, 2, 3, 4, 2}))
+    assert.Equal("", MakePalindrome(""))
+    assert.Equal("catac", MakePalindrome("cat"))
+    assert.Equal("catac", MakePalindrome("cata"))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestRollingMax(t)
+    ExampleTestMakePalindrome(t)
 }
