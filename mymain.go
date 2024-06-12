a3b7d6135e07abcde12a82f47e78fa2a6f781a14
swe-bench <>
2024-06-12 16:19:21 -0400

    Task instance Go/13

--- mymain.go
+++ mymain.go
@@ -4,39 +4,34 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
-// Out of list of strings, return the Longest one. Return the first one in case of multiple
-// strings of the same length. Return nil in case the input list is empty.
-// >>> Longest([])
-// nil
-// >>> Longest(['a', 'b', 'c'])
-// 'a'
-// >>> Longest(['a', 'bb', 'ccc'])
-// 'ccc'
-func Longest(strings []string) interface{}{
 
-    if strings == nil || len(strings) == 0 {
-        return nil
-    }
-    maxlen := 0
-    maxi := 0
-    for i, s := range strings {
-        if maxlen > len(s) {
-            maxlen = len(s)
-            maxi = i
-        }
-    }
-    return strings[maxi]
+// Return a greatest common divisor of two integers a and b
+// >>> GreatestCommonDivisor(3, 5)
+// 1
+// >>> GreatestCommonDivisor(25, 15)
+// 5
+func GreatestCommonDivisor(a int,b int) int{
+
+    if b < 2 {
+		return a
+	}
+	var gcd int = 1
+	for i := 2; i < b; i++ {
+		if a%i == 0 && b%i == 0 {
+			gcd = a
+		}
+	}
+	return gcd
 }
 
-func ExampleTestLongest(t *testing.T) {
-	assert := assert.New(t)
-	assert.Equal(nil, Longest([]string{}))
-	assert.Equal("a", Longest([]string{"a", "b", "c"}))
-	assert.Equal("ccc", Longest([]string{"a", "bb", "ccc"}))
+func ExampleTestGreatestCommonDivisor(t *testing.T) {
+    assert := assert.New(t)
+    assert.Equal(1, GreatestCommonDivisor(3, 5))
+    assert.Equal(5, GreatestCommonDivisor(25, 15))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestLongest(t)
+    ExampleTestGreatestCommonDivisor(t)
 }
