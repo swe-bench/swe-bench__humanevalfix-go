e8a0827cddceac252723145eaebd0c73b08795d7
swe-bench <>
2024-06-12 16:19:36 -0400

    Task instance Go/76

--- mymain.go
+++ mymain.go
@@ -5,50 +5,42 @@ import (
     "github.com/stretchr/testify/assert"
 )
 
-// Write a function that returns true if the given number is the multiplication of 3 prime numbers
-// and false otherwise.
-// Knowing that (a) is less then 100.
-// Example:
-// IsMultiplyPrime(30) == true
-// 30 = 2 * 3 * 5
-func IsMultiplyPrime(a int) bool {
+// Your task is to write a function that returns true if a number x is a simple
+// power of n and false in other cases.
+// x is a simple power of n if n**int=x
+// For example:
+// IsSimplePower(1, 4) => true
+// IsSimplePower(2, 2) => true
+// IsSimplePower(8, 2) => true
+// IsSimplePower(3, 2) => false
+// IsSimplePower(3, 1) => false
+// IsSimplePower(5, 3) => false
+func IsSimplePower(x int,n int) bool {
 
-    isPrime := func(n int) bool {
-        for i := 0; i < int(math.Pow(float64(n), 0.5)+1); i++ {
-            if n%i == 0 {
-                return false
-            }
-        }
-        return true
-    }
-    for i := 2; i < 101; i++ {
-		if !isPrime(i) {
-			continue
-		}
-		for j := 2; j < 101; j++ {
-			if !isPrime(j) {
-				continue
-			}
-			for k := 2; k < 101; k++ {
-				if !isPrime(k) {
-					continue
-				}
-				if i*j*k == a {
-					return true
-				}
-			}
-		}
+    if x == 1 {
+		return true
 	}
-	return false
+	if n==1 {
+		return false
+	}
+	if x % n != 0 {
+		return false
+	}
+	return IsSimplePower(x, n)
 }
 
-func ExampleTestIsMultiplyPrime(t *testing.T) {
+func ExampleTestIsSimplePower(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(true, IsMultiplyPrime(30))
+    assert.Equal(true, IsSimplePower(1, 4))
+    assert.Equal(true, IsSimplePower(2, 2))
+    assert.Equal(true, IsSimplePower(8, 2))
+    assert.Equal(false, IsSimplePower(3, 2))
+    assert.Equal(false, IsSimplePower(3, 1))
+    assert.Equal(false, IsSimplePower(5, 3))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestIsMultiplyPrime(t)
+    ExampleTestIsSimplePower(t)
 }
