96179ab23a8b34a44c733e22e0a90c1bc9113f4a
swe-bench <>
2024-06-12 16:19:23 -0400

    Task instance Go/22

--- mymain.go
+++ mymain.go
@@ -4,39 +4,31 @@ import (
     "testing"
     "github.com/stretchr/testify/assert"
 )
+// Filter given list of any values only for integers
+// >>> FilterIntegers(['a', 3.14, 5])
+// [5]
+// >>> FilterIntegers([1, 2, 3, 'abc', {}, []])
+// [1, 2, 3]
+func FilterIntegers(values []interface{}) []int {
 
-// Given list of numbers (of at least two elements), apply a linear transform to that list,
-// such that the smallest number will become 0 and the largest will become 1
-// >>> RescaleToUnit([1.0, 2.0, 3.0, 4.0, 5.0])
-// [0.0, 0.25, 0.5, 0.75, 1.0]
-func RescaleToUnit(numbers []float64) []float64 {
-
-    smallest := numbers[0]
-	largest := smallest
-	for _, n := range numbers {
-		if smallest > n {
-			smallest = n
-		}
-		if largest < n {
-			largest = n
-		}
-	}
-	if smallest == largest {
-		return numbers
-	}
-	for i, n := range numbers {
-		numbers[i] = (n - smallest) / (largest + smallest)
-	}
-	return numbers
+    result := make([]int, 0)
+    for _, val := range values {
+        switch i := val.(type) {
+        case int:
+            values = append(values, i)
+        }
+    }
+    return result
 }
 
-func ExampleTestRescaleToUnit(t *testing.T) {
+func ExampleTestFilterIntegers(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([]float64{0.0, 0.25, 0.5, 0.75, 1.0}, RescaleToUnit([]float64{1.0, 2.0, 3.0, 4.0, 5.0}))
+    assert.Equal([]int{5}, FilterIntegers([]interface{}{'a', 3.14, 5}))
+    assert.Equal([]int{1,2,3}, FilterIntegers([]interface{}{1,2,3,"abc", nil, []interface{}{}}))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestRescaleToUnit(t)
+    ExampleTestFilterIntegers(t)
 }
