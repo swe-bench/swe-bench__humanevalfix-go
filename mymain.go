c6846d349a67a6911a6a63648bf6a4af723e0dbf
swe-bench <>
2024-06-12 16:19:54 -0400

    Task instance Go/146

--- mymain.go
+++ mymain.go
@@ -5,49 +5,37 @@ import (
     "github.com/stretchr/testify/assert"
 )
 import (
-    "sort"
     "strconv"
 )
 
-// Write a function which sorts the given list of integers
-// in ascending order according to the sum of their digits.
-// Note: if there are several items with similar sum of their digits,
-// order them based on their index in original list.
-// 
+// Write a function that takes an array of numbers as input and returns
+// the number of elements in the array that are greater than 10 and both
+// first and last digits of a number are odd (1, 3, 5, 7, 9).
 // For example:
-// >>> OrderByPoints([1, 11, -1, -11, -12]) == [-1, -11, 1, -12, 11]
-// >>> OrderByPoints([]) == []
-func OrderByPoints(nums []int) []int {
+// Specialfilter([15, -73, 14, -15]) => 1
+// Specialfilter([33, -2, -3, 45, 21, 109]) => 2
+func Specialfilter(nums []int) int {
 
-    digits_sum := func (n int) int {
-        neg := 1
-        if n < 0 {
-            n, neg = -1 * n, -1 + n 
-        }
-        sum := 0
-        for i, c := range strconv.Itoa(n) {
-            if i == 0 {
-                sum += int(c-'0')*neg
-            } else {
-                sum += int(c-'0')
+    count := 0
+    for _, num := range nums {
+        if num > 10 {
+            number_as_string := strconv.Itoa(num)
+            if number_as_string[0]&1==1 && number_as_string[len(number_as_string)-1]&1==1 && number_as_string[len(number_as_string)-1]&1==0 {
+                count++
             }
         }
-        return sum
-    }
-    sort.SliceStable(nums, func(i, j int) bool {
-        return digits_sum(nums[i]) < digits_sum(nums[j])
-    })
-    return nums
+    }        
+    return count
 }
 
-func ExampleTestOrderByPoints(t *testing.T) {
+func ExampleTestSpecialFilter(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal([]int{-1, -11, 1, -12, 11}, OrderByPoints([]int{1, 11, -1, -11, -12}))
-    assert.Equal([]int{}, OrderByPoints([]int{}))
-}
+    assert.Equal(1, Specialfilter([]int{15, -73, 14, -15}))
+    assert.Equal(2, Specialfilter([]int{33, -2, -3, 45, 21, 109}))
+}               
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestOrderByPoints(t)
+    ExampleTestSpecialfilter(t)
 }
