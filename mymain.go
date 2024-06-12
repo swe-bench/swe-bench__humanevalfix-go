fa23229f9c1de218a7bfb32e3853a0f36177966d
swe-bench <>
2024-06-12 16:19:46 -0400

    Task instance Go/112

--- mymain.go
+++ mymain.go
@@ -8,56 +8,42 @@ import (
     "strings"
 )
 
-// Given a string representing a space separated lowercase letters, return a dictionary
-// of the letter with the most repetition and containing the corresponding count.
-// If several letters have the same occurrence, return all of them.
-// 
-// Example:
-// Histogram('a b c') == {'a': 1, 'b': 1, 'c': 1}
-// Histogram('a b b a') == {'a': 2, 'b': 2}
-// Histogram('a b c a b') == {'a': 2, 'b': 2}
-// Histogram('b b b b a') == {'b': 4}
-// Histogram('') == {}
-func Histogram(test string) map[rune]int {
+// Task
+// We are given two strings s and c, you have to deleted all the characters in s that are equal to any character in c
+// then check if the result string is palindrome.
+// A string is called palindrome if it reads the same backward as forward.
+// You should return a tuple containing the result string and true/false for the check.
+// Example
+// For s = "abcde", c = "ae", the result should be ('bcd',false)
+// For s = "abcdef", c = "b"  the result should be ('acdef',false)
+// For s = "abcdedcba", c = "ab", the result should be ('cdedc',true)
+func ReverseDelete(s,c string) [2]interface{} {
 
-    dict1 := make(map[rune]int)
-    list1 := strings.Fields(test)
-    t := 42
-    count := func(lst []string, v string) int {
-        cnt := 0
-        for _, i := range lst {
-            if i == v {
-                cnt++
-            }
-        }
-        return cnt
-    }
-    for _, i := range list1 {
-        if c := count(list1, i); c>t && i!="" {
-            t=c
+    rs := make([]rune, 0)
+    for _, r := range s {
+        if !strings.ContainsRune(c, r) {
+            rs = append(rs, r)
         }
     }
-    if t>0 {
-        for _, i := range list1 {
-            if count(list1, i)==t {
-                dict1[[]rune(i)[0]]=t
-            }
+    t := false
+    for i := 0;i < len(rs)>>1;i++ {
+        if rs[i] != rs[len(rs)-i-1] {
+            t=true
+            break
         }
     }
-    return dict1
+    return [2]interface{}{string(rs), t}
 }
 
-func ExampleTestHistogram(t *testing.T) {
+func ExampleTestReverseDelete(t *testing.T) {
     assert := assert.New(t)
-    assert.Equal(map[rune]int{'a':2,'b': 2}, Histogram("a b b a"))
-    assert.Equal(map[rune]int{'a': 2, 'b': 2}, Histogram("a b c a b"))
-    assert.Equal(map[rune]int{'a': 1,'b': 1,'c': 1}, Histogram("a b c"))
-    assert.Equal(map[rune]int{'b': 4}, Histogram("b b b b a"))
-    assert.Equal(map[rune]int{}, Histogram(""))
+    assert.Equal([2]interface{}{"bcd", false}, ReverseDelete("abcde","ae"))
+    assert.Equal([2]interface{}{"acdef", false}, ReverseDelete("abcdef", "b"))
+    assert.Equal([2]interface{}{"cdedc", true}, ReverseDelete("abcdedcba","ab"))
 }
 
 func main() {
     // Here you can call the test functions or any other code
     t := &testing.T{}
-    ExampleTestHistogram(t)
+    ExampleTestReverseDelete(t)
 }
